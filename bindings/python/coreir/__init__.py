8b4f3bc3b9e4f4a1386f0cc49b297a598eee325b
Lenny Truong <leonard@truong.io>
2017-03-10 10:06:28 -0800

    Init python bindings test

--- bindings/python/coreir/__init__.py
+++ bindings/python/coreir/__init__.py
@@ -1,6 +1,7 @@
 from ctypes import cdll
 import ctypes as ct
 import platform
+import os
 
 
 def load_shared_lib():
@@ -13,7 +14,10 @@ def load_shared_lib():
     else:
         raise NotImplementedError(_system)
 
-    return cdll.LoadLibrary('../../src/coreir.{}'.format(shared_lib_ext))
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+    return cdll.LoadLibrary('{}/../../../build/coreir.{}'.format(dir_path, shared_lib_ext))
 
 class EmptyStruct(ct.Structure):
     pass
@@ -178,39 +182,4 @@ class Context:
 
     def __del__(self):
         coreir_lib.COREDeleteContext(self.context)
-
-if __name__ == "__main__":
-    c = Context()
-    # any = c.Any()
-    # any.print()
-    # c.BitIn().print()
-    # c.BitOut().print()
-    # c.Array(3, c.BitIn()).print()
-
-    # c.Array(3, c.Array(4, c.BitIn())).print()
-
-    # c.ModuleFromFile("test").print()
-    module_typ = c.Record({"input": c.Array(8, c.BitIn()), "output": c.Array(9, c.BitOut())})
-    module = c.G.Module("multiply_by_2", module_typ)
-    module.print()
-    module_def = module.new_definition()
-    add8 = c.G.Module("add8",
-        c.Record({
-            "in1": c.Array(8, c.BitIn()),
-            "in2": c.Array(8, c.BitIn()),
-            "out": c.Array(9, c.BitOut())
-        })
-    )
-    add8_inst = module_def.add_instance_module("adder", add8)
-    add8_in1 = add8_inst.select("in1")
-    add8_in2 = add8_inst.select("in2")
-    add8_out = add8_inst.select("out")
-    interface = module_def.get_interface()
-    _input = interface.select("input")
-    output = interface.select("output")
-    module_def.wire(_input, add8_in1)
-    module_def.wire(_input, add8_in2)
-    module_def.wire(output, add8_out)
-    module.add_definition(module_def)
-    module.print()
 
