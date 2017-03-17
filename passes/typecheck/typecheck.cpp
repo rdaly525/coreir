15f0d1ecf67d756851e9c4450867ec454726099f
James Hegarty <jameshegarty@gmail.com>
2017-03-16 21:48:09 -0700

    added typecheck exe and cgra lib

---
+++ passes/typecheck/typecheck.cpp
@@ -0,0 +1,27 @@
+#include "context.hpp"
+#include "passes.hpp"
+
+using namespace CoreIR;
+
+int main(int argc, char *argv[]){
+  Context* c = newContext();
+
+  if(argc<=1){
+    cout << "usage: typecheck file1.json file2.json ..." << endl;
+    return 1;
+  }
+
+  bool err = false;
+
+  Module* m = loadModule(c,argv[1],&err);
+
+  typecheck(c,m,&err);
+
+  if(err){
+    cout << "failed typechecking!" << endl;
+    return 1;
+  }
+
+  cout << "typecheck OK!" << endl;
+  return 0;
+}
