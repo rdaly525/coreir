11c7cd98037ad6ffa373cd3fb85a7e7ca3752d8e
Ross <rdaly525@stanford.edu>
2017-04-21 14:28:33 -0700

    Made the lib dir contain subdirectories of different libs. coreir-c is the c interface, libs contains any coreir-libs, passes contains any coreir-passes

--- include/coreir-pass/passes.hpp
+++ include/coreir-pass/passes.hpp
@@ -1,7 +1,7 @@
 #ifndef PASSES_HPP_
 #define PASSES_HPP_
 
-#include "../../lib/typecheck.hpp"
-#include "../../lib/rungen.hpp"
+#include "../../lib/passes/typecheck.hpp"
+#include "../../lib/passes/rungen.hpp"
 
 #endif //PASSES_HPP_
