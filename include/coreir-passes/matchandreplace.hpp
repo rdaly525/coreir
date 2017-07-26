e79ddf8a4d9212730d06749a139238fc2b0663e4
Ross <rdaly525@stanford.edu>
2017-07-26 16:38:37 -0700

    Adding forgotten files

---
+++ include/coreir-passes/matchandreplace.hpp
@@ -0,0 +1,59 @@
+#ifndef MATCHANDREPLACE_HPP_
+#define MATCHANDREPLACE_HPP_
+
+#include "coreir.h"
+
+namespace CoreIR {
+
+class MatchAndReplacePass : public ModulePass {
+  public:
+    typedef std::function<Args(const std::vector<Instance*>&)> ConfigArgFun;
+    typedef std::function<bool(const std::vector<Instance*>&)> MatchingCheckFun;
+    struct Opts {
+      Args configargs = Args(); //used if replacement is always a constant configargs
+      Args genargs = Args(); // Used if replacement is a generator
+      std::vector<std::string> instanceKey; //Used for reference in following two functions
+      MatchingCheckFun checkMatching = nullptr; //Checks if a matching pattern is really matching
+      ConfigArgFun getConfigArgs = nullptr; //Calculates the configars based off the matching pattern
+      Opts() {}
+    };
+      
+  private:
+    Module* pattern;
+    Instantiable* replacement;
+    
+    void verifyOpts(Opts opts);
+    
+    Args genargs;
+    Args configargs;
+    ConfigArgFun getConfigArgs; //Can be null
+    MatchingCheckFun checkMatching; //can be null
+
+    //Step 1 stuff
+    
+    //pattern data structures
+    std::vector<std::string> instanceKey;
+    
+    //TODO explain this data type
+    typedef std::vector<std::unordered_map<SelectPath,std::vector<std::pair<SelectPath,uint>>>> InternalConnections;
+    InternalConnections inCons;
+    
+    //TODO explain this too
+    typedef std::vector<std::vector<std::pair<SelectPath,SelectPath>>> ExternalConnections;
+    ExternalConnections exCons;
+    
+    void preprocessPattern();
+
+
+  public:
+    explicit MatchAndReplacePass(Module* pattern, Instantiable* replacement, Opts opts=Opts()) : ModulePass(), pattern(pattern), replacement(replacement), genargs(opts.genargs), configargs(opts.configargs), getConfigArgs(opts.getConfigArgs), checkMatching(opts.checkMatching), instanceKey(opts.instanceKey) {
+      this->verifyOpts(opts);
+      this->preprocessPattern();
+    }
+    bool runOnModule(Module* m);
+
+};
+
+}
+
+#endif
