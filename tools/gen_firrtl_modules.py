d0fd4bd8000d4b3acb4f8bde7fa028d5c19948ec
Lenny Truong <lenny@cs.stanford.edu>
2017-08-10 20:52:32 -0700

    Interpret SInt results as UInts, more explicit output file name

--- tools/gen_firrtl_modules.py
+++ tools/gen_firrtl_modules.py
@@ -25,20 +25,20 @@ ops = {
         "xor"   : "xor(in0, in1)",
         "dshl"  : "dshl(in0, in1)",
         "dlshr" : "dshr(in0, in1)",
-        "dashr" : "dshr(asSInt(in0), in1)",
+        "dashr" : "asUInt(dshr(asSInt(in0), in1))",
         "add"   : "add(in0, in1)",
         "sub"   : "sub(in0, in1)",
         "mul"   : "mul(in0, in1)",
         "udiv"  : "div(in0, in1)",
         "urem"  : "mod(in0, in1)",
-        "sdiv"  : "div(asSInt(in0), asSInt(in1))",
-        "srem"  : "mod(asSInt(in0), asSInt(in1))", 
+        "sdiv"  : "asUInt(div(asSInt(in0), asSInt(in1)))",
+        "srem"  : "asUInt(mod(asSInt(in0), asSInt(in1)))", 
         # "smod"  : "$signed(in0) % $signed(in1)", #TODO definitely wrong
         "eq"  : "eq(in0, in1)",
-        "slt" : "lt(asSInt(in0),  asSInt(in1))",
-        "sgt" : "gt(asSInt(in0),  asSInt(in1))",
-        "sle" : "leq(asSInt(in0), asSInt(in1))",
-        "sge" : "gte(asSInt(in0), asSInt(in1))",
+        "slt" : "asUInt(lt(asSInt(in0),  asSInt(in1)))",
+        "sgt" : "asUInt(gt(asSInt(in0),  asSInt(in1)))",
+        "sle" : "asUInt(leq(asSInt(in0), asSInt(in1)))",
+        "sge" : "asUInt(gte(asSInt(in0), asSInt(in1)))",
         "ult" : "lt(in0, in1)",
         "ugt" : "gt(in0, in1)",
         "ule" : "leq(in0, in1)",
@@ -69,6 +69,6 @@ for _type, ops_set in ops.items():
         source.add_line()
 
 
-with open("primitive_wrappers.fir", "w") as output:
+with open("coreir_primitive_wrappers.fir", "w") as output:
     output.write(str(source))
 
