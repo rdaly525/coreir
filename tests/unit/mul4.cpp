32ee5aa8efd197cdf20dba2de3e5b732ee816e84
Dillon Huff <dillonhuff@gmail.com>
2017-08-17 16:59:59 -0700

    Now have a mul4 example that seems to run

--- tests/unit/mul4.cpp
+++ tests/unit/mul4.cpp
@@ -5,5 +5,55 @@
 using namespace CoreIR;
 
 int main() {
-  assert(false);
+  uint n = 64;
+
+  Context* c = newContext();
+
+  Namespace* g = c->getGlobal();
+
+  Namespace* stdlib = CoreIRLoadLibrary_stdlib(c);
+
+  // Declare mul generator
+  Generator* mul2 = stdlib->getGenerator("mul");
+  assert(mul2);
+
+  // Define mul4 module
+  Type* mul4Type = c->Record({
+      {"in", c->Array(4, c->Array(n, c->BitIn()))},
+	{"out", c->Array(n, c->Bit())}
+    });
+
+  Module* mul4_n = g->newModuleDecl("Mul4", mul4Type);
+  ModuleDef* def = mul4_n->newModuleDef();
+
+  // Create wireables
+  Wireable* self = def->sel("self");
+
+  Wireable* mul_00 = def->addInstance("mul00", mul2, {{"width", c->argInt(n)}});
+  Wireable* mul_01 = def->addInstance("mul01", mul2, {{"width", c->argInt(n)}});
+  Wireable* mul_1 = def->addInstance("mul1", mul2, {{"width", c->argInt(n)}});
+
+  def->connect(self->sel("in")->sel(0), mul_00->sel("in")->sel(0));
+  def->connect(self->sel("in")->sel(1), mul_00->sel("in")->sel(1));
+
+  def->connect(self->sel("in")->sel(2), mul_01->sel("in")->sel(0));
+  def->connect(self->sel("in")->sel(3), mul_01->sel("in")->sel(1));
+
+  def->connect(mul_00->sel("out"), mul_1->sel("in")->sel(0));
+  def->connect(mul_01->sel("out"), mul_1->sel("in")->sel(1));
+
+  def->connect(mul_1->sel("out"), self->sel("out"));
+
+  mul4_n->setDef(def);
+  mul4_n->print();
+
+  bool err = false;
+
+  cout << "Checking saving and loading pregen" << endl;
+  saveModule(mul4_n, "_mul4.json", &err);
+  if (err) {
+    cout << "Could not save to json!!" << endl;
+    c->die();
+  }
+  
 }
