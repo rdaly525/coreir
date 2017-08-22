9d8cfda601d87aef5662a4a4d9e4a941c6f113fb
Cristian Mattarei <cristian.mattarei@gmail.com>
2017-08-22 16:06:07 -0700

    Added implementation of smtmodule

---
+++ src/passes/analysis/smtlib2.cpp
@@ -0,0 +1,76 @@
+#include "coreir.h"
+#include "coreir-passes/analysis/smtmodule.hpp"
+#include "coreir-passes/analysis/smtlib2.h"
+
+using namespace CoreIR;
+
+namespace {
+
+string SMTWireDec(SMTWire w) { return "  wire " + w.dimstr() + " " + w.getName() + ";"; }
+
+
+string VAssign(Connection con) {
+  Wireable* left = con.first->getType()->getDir()==Type::DK_In ? con.first : con.second;
+  Wireable* right = left==con.first ? con.second : con.first;
+  SMTWire vleft(left);
+  SMTWire vright(right);
+  return "  (= " + vleft.getName() + " = " + vright.getName() + ")";
+}
+
+}
+
+std::string Passes::SmtLib2::ID = "smtlib2";
+bool Passes::SmtLib2::runOnInstanceGraphNode(InstanceGraphNode& node) {
+  
+  //Create a new SMTmodule for this node
+  Instantiable* i = node.getInstantiable();
+  if (auto g = dyn_cast<Generator>(i)) {
+    this->modMap[i] = new SMTModule(g);
+    this->external.insert(i);
+    return false;
+  }
+  Module* m = cast<Module>(i);
+  SMTModule* vmod = new SMTModule(m);
+  modMap[i] = vmod;
+  if (!m->hasDef()) {
+    this->external.insert(i);
+    return false;
+  }
+
+  ModuleDef* def = m->getDef();
+  
+  string tab = "  ";
+  for (auto imap : def->getInstances()) {
+    string iname = imap.first;
+    Instance* inst = imap.second;
+    Instantiable* iref = imap.second->getInstantiableRef();
+    vmod->addStmt("  //Wire declarations for instance '" + imap.first + "' (Module "+ iref->getName() + ")");
+    for (auto rmap : cast<RecordType>(imap.second->getType())->getRecord()) {
+      vmod->addStmt(SMTWireDec(SMTWire(iname+"_"+rmap.first,rmap.second)));
+    }
+    ASSERT(modMap.count(iref),"DEBUG ME: Missing iref");
+    vmod->addStmt(modMap[iref]->toInstanceString(inst));
+  }
+
+  vmod->addStmt("  //All the connections");
+  for (auto con : def->getConnections()) {
+    vmod->addStmt(VAssign(con));
+  }
+  
+  return false;
+}
+
+void Passes::SmtLib2::writeToStream(std::ostream& os) {
+  
+  for (auto ext : external) {
+    os << modMap[ext]->toCommentString() << endl;
+  }
+  os << endl;
+  for (auto mmap : modMap) {
+    if (external.count(mmap.first)==0) { 
+      os << mmap.second->toString() << endl;
+    }
+  }
+
+
+}
---
+++ src/passes/analysis/smtmodule.cpp
@@ -0,0 +1,86 @@
+#include "coreir.h"
+#include "coreir-passes/analysis/smtmodule.hpp"
+
+using namespace CoreIR;
+
+string SMTModule::toString() {
+  vector<string> pdecs;
+  for (auto pmap : ports) {
+    auto port = pmap.second;
+    pdecs.push_back(port.dirstr() + " " + port.dimstr() + " " + port.getName());
+  }
+  ostringstream o;
+  string tab = "  ";
+  //Module declaration
+  o << endl << "module " << modname << "(\n" << tab << join(pdecs.begin(),pdecs.end(),string(",\n  ")) << "\n);" << endl;
+
+  //Param declaraions
+  for (auto p : params) {
+    o << tab << "parameter " << p;
+    if (paramDefaults.count(p)) {
+      o << " = " << paramDefaults[p];
+    }
+    o << ";" << endl;
+  }
+  o << endl;
+  
+  for (auto s : stmts) o << s << endl;
+  o << endl << "endmodule //" << modname;
+  return o.str();
+}
+
+string SMTModule::toInstanceString(Instance* inst) {
+  string instname = inst->getInstname();
+  Instantiable* iref = inst->getInstantiableRef();
+  if (this->gen) {
+    ASSERT(inst->isGen(),"DEBUG ME:");
+  }
+  ostringstream o;
+  string tab = "  ";
+  string mname;
+  unordered_map<string,SMTWire> iports;
+  Args args;
+  if (gen) {
+    args = inst->getGenArgs();
+    Type2Ports(gen->getTypeGen()->getType(inst->getGenArgs()),iports);
+    mname = gen->getNamespace()->getName() + "_" + gen->getName(args);
+  }
+  else {
+    mname = modname;
+    iports = ports;
+  }
+
+  for (auto amap : inst->getConfigArgs()) {
+    ASSERT(args.count(amap.first)==0,"NYI Alisaaed config/genargs");
+    args[amap.first] = amap.second;
+  }
+  o << tab << mname << " ";
+  vector<string> params;
+  const json& jmeta = iref->getMetaData();
+  if (jmeta.count("verilog") && jmeta["verilog"].count("parameters")) {
+    params = jmeta["verilog"]["parameters"].get<vector<string>>();
+  }
+  else {
+    for (auto amap : args) {
+      params.push_back(amap.first);
+    }
+  }
+  vector<string> paramstrs;
+  for (auto param : params) {
+    ASSERT(args.count(param),"Missing parameter " + param + " from " + Args2Str(args));
+    string astr = "." + param + "(" + args[param]->toString() + ")";
+    paramstrs.push_back(astr);
+  }
+  if (paramstrs.size()) {
+    o << "#(" << join(paramstrs.begin(),paramstrs.end(),string(",")) << ") ";
+  }
+  //Assume names are <instname>_port
+  vector<string> portstrs;
+  for (auto port : iports) {
+    string pstr = "."+port.first+"(" + instname+"_"+ port.first+")";
+    portstrs.push_back(pstr);
+  }
+  o << instname << "(\n" << tab << tab << join(portstrs.begin(),portstrs.end(),",\n"+tab+tab) << "\n  );" << endl;
+  return o.str();
+}
+
