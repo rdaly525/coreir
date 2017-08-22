e06de5e9e2e204c664be8816cfd1c6c6c5169fac
Lenny Truong <lenny@cs.stanford.edu>
2017-08-21 23:25:56 -0600

    ice40 FullAdder yosys flow

--- examples/ice40/FullAdder.v
+++ examples/ice40/FullAdder.v
@@ -1,6 +1,6 @@
-//Module: ice40_SB_LUT4 defined externally
 //Module: coreir_const defined externally
-//Module: ice40_SB_CARRY defined externally
+//Module: SB_LUT4 defined externally
+//Module: SB_CARRY defined externally
 
 
 module FullAdder(
@@ -12,29 +12,29 @@ module FullAdder(
 );
 
   //Wire declarations for instance 'inst1' (Module SB_CARRY)
-  wire  inst1_I1;
-  wire  inst1_CO;
   wire  inst1_I0;
   wire  inst1_CI;
-  ice40_SB_CARRY inst1(
-    .I1(inst1_I1),
-    .CO(inst1_CO),
+  wire  inst1_I1;
+  wire  inst1_CO;
+  SB_CARRY inst1(
+    .CI(inst1_CI),
     .I0(inst1_I0),
-    .CI(inst1_CI)
+    .CO(inst1_CO),
+    .I1(inst1_I1)
   );
 
   //Wire declarations for instance 'inst0' (Module SB_LUT4)
-  wire  inst0_O;
-  wire  inst0_I3;
-  wire  inst0_I2;
-  wire  inst0_I1;
   wire  inst0_I0;
-  ice40_SB_LUT4 #(.LUT_INIT(38550)) inst0(
-    .O(inst0_O),
-    .I3(inst0_I3),
-    .I1(inst0_I1),
+  wire  inst0_I1;
+  wire  inst0_I2;
+  wire  inst0_I3;
+  wire  inst0_O;
+  SB_LUT4 #(.LUT_INIT(38550)) inst0(
+    .I0(inst0_I0),
     .I2(inst0_I2),
-    .I0(inst0_I0)
+    .I1(inst0_I1),
+    .O(inst0_O),
+    .I3(inst0_I3)
   );
 
   //Wire declarations for instance 'const_inst0' (Module const)
@@ -44,12 +44,12 @@ module FullAdder(
   );
 
   //All the connections
-  assign inst1_I0 = I0;
-  assign inst1_CI = CIN;
+  assign inst0_I3 = const_inst0_out;
   assign inst1_I1 = I1;
-  assign inst0_I3 = const_inst0_out[0];
   assign inst0_I1 = I1;
   assign inst0_I0 = I0;
+  assign inst1_I0 = I0;
+  assign inst1_CI = CIN;
   assign inst0_I2 = CIN;
   assign O = inst0_O;
   assign COUT = inst1_CO;
