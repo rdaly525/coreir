782b9d77be81fe459e1d6f504537fb0846084fe1
Dillon Huff <dillonhuff@gmail.com>
2017-09-05 16:40:59 -0700

    Now moving test harnesses to cpp

---
+++ tests/simulator/gencode/test_sle7.cpp
@@ -0,0 +1,30 @@
+#include "sle7.h"
+
+#include <cstdio>
+
+int main() {
+  uint8_t a[2];
+  a[0] = 1 << 6;
+  a[1] = 1;
+
+  /* int res = ((int8_t) a[0]) <= ((int8_t) a[1]); */
+
+  /* printf("sle res = %d\n", res); */
+
+  // 1 << 6 is the smallest possible 7 bit 2s complement number
+  uint8_t expected = 1;
+
+  printf("expected as long before calling simulate = %hhu\n", expected);
+
+  uint8_t r = 10;
+  simulate(a, &r);
+
+  printf("expected as long = %hhu\n", expected);
+  printf("result   as long = %hhu\n", r);
+  
+  if (expected == r) {
+    return 0;
+  }
+
+  return 1;
+}
