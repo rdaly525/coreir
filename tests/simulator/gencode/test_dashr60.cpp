2a37acf44c399c475b23b9f19f969d2d5970ba0b
Dillon Huff <dillonhuff@gmail.com>
2017-09-07 16:34:31 -0700

    Added test_dashr60 to repo

---
+++ tests/simulator/gencode/test_dashr60.cpp
@@ -0,0 +1,40 @@
+#include "dashr60.h"
+
+#include <bitset>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  uint64_t A1[2];
+  A1[0] = 1ULL << 59;
+  A1[1] = 3ULL;
+
+  uint64_t expected1 = (1ULL << 59) | (1ULL << 58) | (1ULL << 57) | (1ULL << 56);
+  uint64_t res1 = 34;
+
+  simulate(A1, &res1);
+
+  cout << "Expected1 = " << std::bitset<64>(expected1) << endl;
+  cout << "Result1   = " << std::bitset<64>(res1) << endl;
+
+  uint64_t A2[2];
+  A2[0] = 1ULL << 58;
+  A2[1] = 5ULL;
+  
+  uint64_t expected2 = (1ULL << 53);
+  uint64_t res2 = 23;
+
+  simulate(A2, &res2);
+
+  cout << "Expected2 = " << std::bitset<64>(expected2) << endl;
+  cout << "Result2   = " << std::bitset<64>(res2) << endl;
+  
+  if ((expected1 == res1) && (expected2 == res2)) {
+    return 0;
+  }
+
+  return 1;
+
+}
+//correct mask uint64_t dmask = (((1ULL << (self_A[1])) - 1) << (59 - self_A[1] + 1));
