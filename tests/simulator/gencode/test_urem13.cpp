e7fc0f4b6cc92b778c2950441bb96db12c568c11
Dillon Huff <dillonhuff@gmail.com>
2017-09-09 19:37:31 -0700

    urem is passing tests

--- tests/simulator/gencode/test_urem13.cpp
+++ tests/simulator/gencode/test_urem13.cpp
@@ -1,5 +1,18 @@
 #include "urem13.h"
 
 int main() {
-  return 0;
+  uint16_t A[2];
+  A[0] = 302;
+  A[1] = 3;
+
+  uint16_t res = 345;
+  uint16_t expected = 2;
+
+  simulate(A, &res);
+
+  if (res == expected) {
+    return 0;
+  }
+
+  return 1;
 }
