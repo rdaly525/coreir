ddab5e5c2d175c897172bebb26d686197961c5fd
Cristian Mattarei <mattarei@stanford.edu>
2017-09-14 14:20:42 -0700

    Added counter with design bug. Revised nuXmv scripts

--- tools/nuXmv_check.sh
+++ tools/nuXmv_check.sh
@@ -1,2 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-nuXmv -cpp -source $DIR/nuXmv_commands.txt $1
+if [ "$#" -eq 2 ];
+then
+    nuXmv -cpp -bmc -bmc_length $1 -source $DIR/nuXmv_commands.txt $2
+else
+    nuXmv -cpp -source $DIR/nuXmv_commands.txt $1
+fi
