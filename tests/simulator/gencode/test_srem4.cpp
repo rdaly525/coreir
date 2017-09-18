527a5ff442a353a31e05619f466b27b70439d803
Dillon Huff <dillonhuff@gmail.com>
2017-09-18 11:15:19 -0700

    All tests pass

--- tests/simulator/gencode/test_srem4.cpp
+++ tests/simulator/gencode/test_srem4.cpp
@@ -7,17 +7,23 @@ using namespace std;
 
 int main() {
   uint8_t A[3];
-  A[0] = 19;
-  A[1] = 4;
-  A[2] = -7;
+  A[0] = 27;
+  A[1] = 14;
+  // -15 in 6 bit signed
+  A[2] = 1 | (1 << 4) | (1 << 5);
 
-  // -15 % 2 == -1
-  uint8_t expected = 0b11111;
+
+  // -1
+  uint8_t expected = 0b1;
 
   uint8_t res = 234;
 
-  cout << "3 srem before      = " << bitset<8>(expected) << endl;
-  cout << "3 srem before      = " << bitset<8>(res) << endl;
+  cout << "3 srem before       = " << bitset<8>(expected) << endl;
+  cout << "3 srem before       = " << bitset<8>(res) << endl;
+
+  cout << "A0 srem before      = " << bitset<8>(A[0]) << endl;
+  cout << "A1 srem before      = " << bitset<8>(A[1]) << endl;
+  cout << "A2 srem before      = " << bitset<8>(A[2]) << endl;
   
   simulate(&res, A);
 
