b40df76f64ae01f3305cca0ca65729ac22bbd3aa
Dillon Huff <dillonhuff@gmail.com>
2017-09-22 10:33:22 -0700

    15 failed

--- tests/simulator/gencode/test_add63.cpp
+++ tests/simulator/gencode/test_add63.cpp
@@ -6,21 +6,27 @@
 int main() {
   uint64_t one = 1;
 
-  uint64_t ins[2];
-  ins[0] = (one << 62);
-  ins[1] = (one << 62);
+  circuit_state state;
+  state.self_input[0] = one << 62;
+  state.self_input[1] = one << 62;
 
-  uint64_t out;
+  // uint64_t ins[2];
+  // ins[0] = (one << 62);
+  // ins[1] = (one << 62);
 
-  simulate(ins, &out);
+  // uint64_t out;
+
+  //simulate(ins, &out);
+
+  simulate(&state);
 
   // NOTE: 37th and 60th bits should be masked away by the simulator
   uint64_t expected = 0;
   
-  printf("and out  = %lu\n", out);
-  printf("expected = %lu\n", expected);
+  // printf("and out  = %lu\n", out);
+  // printf("expected = %lu\n", expected);
 
-  if (expected == out) {
+  if (expected == state.self_output) {
     return 0;
   }
 
