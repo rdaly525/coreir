4665b0d7020202ee0bcd06e92368cd707fe28f35
Dillon Huff <dillonhuff@gmail.com>
2017-09-22 11:02:25 -0700

    4 cases failing. The combinational cases are all fixed

--- tests/simulator/gencode/test_sdiv5.cpp
+++ tests/simulator/gencode/test_sdiv5.cpp
@@ -6,21 +6,25 @@
 using namespace std;
 
 int main() {
-  uint8_t A[2];
-  A[0] = (1 << 4) + 1;
-  A[1] = 2;
+  circuit_state state;
+  //  uint8_t A[2];
+  state.self_A[0] = (1 << 4) + 1;
+  state.self_A[1] = 2;
 
   // -15 / 2 == -7
   uint8_t expected = 0b11001;
 
-  uint8_t res = 234;
+  //uint8_t res = 234;
+  state.self_out = 234;
 
-  simulate(&res, A);
+  //simulate(&res, A);
+
+  simulate(&state);
 
   cout << "sdiv expected = " << bitset<8>(expected) << endl;
-  cout << "sdiv res      = " << bitset<8>(res) << endl;
+  //cout << "sdiv res      = " << bitset<8>(res) << endl;
 
-  if (expected == res) {
+  if (expected == state.self_out) {
     return 0;
   }
 
