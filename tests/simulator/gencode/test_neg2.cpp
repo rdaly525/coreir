df7cac5251b7369593fbc378de2ee4926bce5138
Dillon Huff <dillonhuff@gmail.com>
2017-09-22 10:35:38 -0700

    14 failed

--- tests/simulator/gencode/test_neg2.cpp
+++ tests/simulator/gencode/test_neg2.cpp
@@ -7,13 +7,20 @@ int main() {
 
   uint8_t expected = 1 << 1;
 
+  circuit_state state;
+  state.self_A = 1;
+  state.self_res = 23;
+
   printf("Neg2 before simulating\n");
-  simulate(&res, a);
 
-  printf("2 bit not expected = %c\n", expected);
-  printf("2 bit not result   = %c\n", res);
+  //simulate(&res, a);
 
-  if (res == expected) {
+  simulate(&state);
+
+  // printf("2 bit not expected = %c\n", expected);
+  // printf("2 bit not result   = %c\n", res);
+
+  if (state.self_res == expected) {
     return 0;
   }
 
