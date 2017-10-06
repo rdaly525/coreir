762ab60073f8861d6aa99990eed7b4dbad6e2f76
Lenny Truong <lenny@cs.stanford.edu>
2017-10-06 09:56:09 -0700

    move type_gen decorator to pycoreir

--- tests/unit/add.py
+++ tests/unit/add.py
@@ -1,21 +1,6 @@
 import coreir
-import ctypes
-from functools import wraps
 
-def type_gen(fn):
-    @wraps(fn)
-    def wrapped(context, names, values, num_values):
-        context = coreir.Context(ctypes.cast(context, coreir.COREContext_p))
-        values = ctypes.cast(values, ctypes.POINTER(coreir.COREValue_p))
-        names = ctypes.cast(names, ctypes.POINTER(ctypes.c_char_p))
-        values_map = {}
-        for i in range(num_values):
-            values_map[names[i]] = coreir.Value(values[i], context)
-        type_obj = fn(context, values_map)
-        return ctypes.addressof(type_obj.ptr.contents)
-    return wrapped
-
-@type_gen
+@coreir.type_gen
 def add_type_gen(context, values):
     width = values['width'].value
     N = values['N'].value
