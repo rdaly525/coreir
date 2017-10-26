ab05e742facf0f44274a55f885b2be10d6b8dc46
Lenny Truong <lenny@cs.stanford.edu>
2017-10-25 23:50:08 -0700

    Only set rpath for dylib, PYTHONHOME logic fix

--- include/coreir/ir/typegen-python.h
+++ include/coreir/ir/typegen-python.h
@@ -22,9 +22,9 @@ class TypeGenFromPython : public TypeGen {
       Py_Initialize();
       // Add cwd to path (not added by default when using the embedded
       // interpreter)
-      PyRun_SimpleString( "import sys\nsys.path.append(\".\")\n" );
       wchar_t python_home[] = PYTHON_HOME;
       Py_SetPythonHome(python_home);
+      PyRun_SimpleString( "import sys\nsys.path.append(\".\")\n" );
       PyObject *py_module = PyImport_ImportModule(moduleName.c_str());
       if (py_module != NULL) {
         Py_INCREF(py_module);
