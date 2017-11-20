ee82a0221fb34fe49824dd970d671390086f5643
Dillon Huff <dillonhuff@gmail.com>
2017-11-20 14:57:19 -0600

    Stabilizing combinational logic before updating clock values

--- tests/simulator/gencode/test_comb_then_reg.cpp
+++ tests/simulator/gencode/test_comb_then_reg.cpp
@@ -25,9 +25,25 @@ int main() {
 
   simulate(&state);
 
+  assert(state.self_out_0 == 2);
+  assert(state.self_out_1 == 2);
+  
   cout << "After second high clock" << endl;
   cout << "state.self_out_0 = " << bitset<8>(state.self_out_0) << endl;
   cout << "state.self_out_1 = " << bitset<8>(state.self_out_1) << endl;
 
-  return 1;
+  state.self_in_0 = 0;
+  state.self_clk = 0;
+  state.self_clk_last = 1;
+
+  simulate(&state);
+
+  assert(state.self_out_0 == 1);
+  assert(state.self_out_1 == 2);
+  
+  cout << "After falling clock edge" << endl;
+  cout << "state.self_out_0 = " << bitset<8>(state.self_out_0) << endl;
+  cout << "state.self_out_1 = " << bitset<8>(state.self_out_1) << endl;
+  
+  return 0;
 }
