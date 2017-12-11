74b053514d9651823dc227cbc6a12355a1db0b57
Dillon Huff <dillonhuff@gmail.com>
2017-12-11 00:10:35 -0800

    Can now print out non-clock outputs

--- src/passes/transform/registerinputs.cpp
+++ src/passes/transform/registerinputs.cpp
@@ -9,6 +9,13 @@ bool Passes::RegisterInputs::runOnInstanceGraphNode(InstanceGraphNode& node) {
   if (!module->hasDef()) {
     return false;
   }
+
+  Context* c = module->getDef()->getContext();
+  for (auto& field : module->getType()->getRecord()) {
+    if (field.second != c->Named("coreir.clkIn")) {
+      cout << "Input = " << field.first << endl;
+    }
+  }
   
   return false;
 }
