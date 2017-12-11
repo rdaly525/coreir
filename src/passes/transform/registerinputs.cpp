4381ceabd2d32ed39c0b34dff380831afab14876
Dillon Huff <dillonhuff@gmail.com>
2017-12-11 01:36:49 -0800

    Not sure the most efficient way to write a register replacement pass

--- src/passes/transform/registerinputs.cpp
+++ src/passes/transform/registerinputs.cpp
@@ -10,12 +10,49 @@ bool Passes::RegisterInputs::runOnInstanceGraphNode(InstanceGraphNode& node) {
     return false;
   }
 
+  ModuleDef* def = module->getDef();
+
   Context* c = module->getDef()->getContext();
   for (auto& field : module->getType()->getRecord()) {
     if (field.second != c->Named("coreir.clkIn")) {
-      cout << "Input = " << field.first << endl;
+
+      Type::DirKind dk = field.second->getDir();
+
+      if (dk == Type::DirKind::DK_In) {
+        //Wireable* w = field.first;
+        cout << "Input = " << field.first << endl;
+
+        // cout << "With connections" << endl;
+        // for (auto& connected : w->getConnectedWireables()) {
+        //   cout << connected->toString() << endl;
+        // }
+      }
     }
   }
+
+  auto interface = def->getInterface();
+  cout << interface->getType()->toString() << endl;
+
+  cout << "# of wireables connected to interfaces = " << interface->getConnectedWireables().size() << endl;
+  for (auto& wd : interface->getConnectedWireables()) {
+    cout << wd->toString() << endl;
+  }
+
+  Wireable* self = def->sel("self");
+  cout << "# of wireables connected to self = " << self->getConnectedWireables().size() << endl;
+  for (auto& wd : self->getConnectedWireables()) {
+    cout << wd->toString() << endl;
+  }
+
+  map<Wireable*, Instance*> newRegs;
+  for (auto& conn : def->getConnections()) {
+    cout << Connection2Str(conn) << endl;
+
+    // Wireable* sel1 = conn.first;
+    // Wireable* sel2 = conn.second;
+
+    
+  }
   
-  return false;
+  return true;
 }
