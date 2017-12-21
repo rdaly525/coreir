e3cb3bc6ea3744ab5ba878b456ae9c8e22f3b37f
Dillon Huff <dillonhuff@gmail.com>
2017-12-20 23:49:36 -0800

    Need to find or make a connection grouping unit test

--- src/passes/transform/groupconnections.cpp
+++ src/passes/transform/groupconnections.cpp
@@ -12,5 +12,37 @@ bool Passes::GroupConnections::runOnModule(Module* m) {
     return false;
   }
 
+  ModuleDef* def = m->getDef();
+  cout << "Connections" << endl;
+  vector<Connection> arrayToArrayConns;
+  for (auto& conn : def->getConnections()) {
+
+
+    Wireable* l = conn.first;
+    Wireable* r = conn.second;
+
+    if (isa<Select>(l) && isa<Select>(r)) {
+      Select* ls = cast<Select>(l);
+      Select* rs = cast<Select>(r);
+
+      if (isNumber(ls->getSelStr()) &&
+          isNumber(rs->getSelStr())) {
+
+        auto lsTp = cast<ArrayType>(ls->getParent()->getType());
+        auto rsTp = cast<ArrayType>(rs->getParent()->getType());
+
+        cout << ls->getParent()->toString() << " len = " << lsTp->getLen() << endl;
+        cout << rs->getParent()->toString() << " len = " << rsTp->getLen() << endl;
+        if (lsTp->getLen() == rsTp->getLen()) {
+          arrayToArrayConns.push_back(conn);
+          cout << "( " << ls->toString() << ", " << rs->toString() << " )" << endl;
+        }
+      }
+    }
+  }
+
+  cout << "# of array - array connections = " << arrayToArrayConns.size() << endl;
+  vector<vector<Connection> > cLists;
+    
   return false;
 }
