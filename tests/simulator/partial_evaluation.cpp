66c1af0f50677885e06e604cc3b1e38ab3fbbf71
Dillon Huff <dillonhuff@gmail.com>
2017-12-30 14:55:56 -0700

    Can now generate circuit state that I want. Will need a more general evaluator for code with loops

--- tests/simulator/partial_evaluation.cpp
+++ tests/simulator/partial_evaluation.cpp
@@ -13,6 +13,43 @@ using namespace std;
 
 namespace CoreIR {
 
+  class PartialEvaluator {
+    CoreIR::Module* mod;
+
+    CircuitState last;
+    CircuitState current;
+
+  public:
+    PartialEvaluator(Module* const mod_) : mod(mod_) {}
+
+    void setValue(const std::string& name,
+                  const BitVec& bv) {
+      ModuleDef* def = mod->getDef();
+      Wireable* w = def->sel(name);
+      Select* s = toSelect(w);
+
+      setValue(s, bv);
+    }
+
+    void setValue(CoreIR::Select* const sel,
+                  const BitVec& bv) {
+    }
+    
+    void eval() {
+
+      // Update comb logic
+      // Update clocks
+      // Update comb logic again
+      
+      last = current;
+    }
+  };
+
+  void registersToConstants(CoreIR::Module* const mod,
+                            std::unordered_map<std::string, BitVec>& regValues) {
+    
+  }
+
   TEST_CASE("Partial evaluation") {
     Context* c = newContext();
 
@@ -22,10 +59,43 @@ namespace CoreIR {
       Type* regOut =
         c->Record({{"clk", c->Named("coreir.clkIn")},
               {"in", c->BitIn()->Arr(width)},
+                {"en", c->BitIn()},
                 {"out", c->Bit()->Arr(width)}});
 
 
-      REQUIRE(false);
+      Module* rg = c->getGlobal()->newModuleDecl("rg", regOut);
+      ModuleDef* def = rg->newModuleDef();
+
+      def->addInstance("r", "mantle.reg", {{"width", Const::make(c, width)}, {"has_en", Const::make(c, true)}});
+
+      def->connect("self.en", "r.en");
+      def->connect("self.clk", "r.clk");
+      def->connect("self.in", "r.in");
+      def->connect("r.out", "self.out");
+
+      rg->setDef(def);
+
+      // PartialEvaluator pe(rg);
+      // pe.setValue("self.clk", BitVec(1, 0));
+      // pe.eval();
+
+      // pe.setValue("self.clk", BitVec(1, 1));
+      // pe.setValue("self.en", BitVec(1, 1));
+      // pe.setValue("self.in", BitVec(width, 5));
+
+      // pe.eval();
+
+      c->runPasses({"rungenerators", "flattentypes", "flatten"});
+
+      SimulatorState state(rg);
+      state.setClock("self.clk", 0, 1);
+      state.setValue("self.en", BitVec(1, 1));
+      state.setValue("self.in", BitVec(width, 56));
+
+      state.execute();
+      state.execute();
+      
+      REQUIRE(state.getBitVec("self.out") == BitVec(width, 56));
     }
 
     deleteContext(c);
