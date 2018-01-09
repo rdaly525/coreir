c1d55b42af8c48c204b0013ba59811a421ce4f3d
Dillon Huff <dillonhuff@gmail.com>
2018-01-09 12:01:18 -0800

    cullzexts passes a basic test

--- src/passes/transform/cullzexts.cpp
+++ src/passes/transform/cullzexts.cpp
@@ -11,5 +11,43 @@ bool Passes::CullZexts::runOnModule(Module* m) {
     return false;
   }
 
-  return true;
+  auto def = m->getDef();
+
+  bool deletedZext = false;
+  for (auto instS : def->getInstances()) {
+    Instance* inst = instS.second;
+
+    if (getQualifiedOpName(*inst) == "coreir.zext") {
+      auto args = inst->getModuleRef()->getGenArgs();
+
+      uint in_width = args.at("width_in")->get<int>();
+      uint out_width = args.at("width_out")->get<int>();
+
+      if (in_width == out_width) {
+        // Found useless zext
+        cout << inst->toString() << " is an identity zext" << endl;
+
+        Select* inSel = inst->sel("in");
+        Select* outSel = inst->sel("out");
+
+        // Only handling easy wiring case for now
+        if ((inSel->getConnectedWireables().size() == 1) &&
+            (outSel->getConnectedWireables().size() == 1)) {
+
+          cout << "Deleting " << inst->toString() << endl;
+
+          Select* toIn = cast<Select>(*std::begin(inSel->getConnectedWireables()));
+          Select* toOut = cast<Select>(*std::begin(outSel->getConnectedWireables()));
+
+          def->removeInstance(inst);
+
+          def->connect(toIn, toOut);
+
+          deletedZext = true;
+        }
+      }
+    }
+  }
+
+  return deletedZext;
 }
