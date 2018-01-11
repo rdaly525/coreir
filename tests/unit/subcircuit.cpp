8e31adb3c9c688659505742785166eb0a8c0db04
Dillon Huff <dillonhuff@gmail.com>
2018-01-11 14:29:42 -0800

    Register that should not be included in test example is included

--- tests/unit/subcircuit.cpp
+++ tests/unit/subcircuit.cpp
@@ -148,8 +148,101 @@ void testNodeAfterConstant() {
 
 }
 
+void testSubcircuitModule() {
+  Context* c = newContext();
+
+  Namespace* g = c->getGlobal();
+
+  uint width = 32;
+  uint addr_width = 16;
+
+  Type* miniChipType = c->Record({
+      {"in0", c->BitIn()->Arr(width)},
+        {"in1", c->BitIn()->Arr(width)},
+          {"config_addr", c->BitIn()->Arr(addr_width)},
+            {"config_data", c->BitIn()},
+              {"clk", c->Named("coreir.clkIn")},
+                {"out", c->Bit()->Arr(width)}
+    });
+
+  Module* miniChip = g->newModuleDecl("miniChip", miniChipType);
+  ModuleDef* def = miniChip->newModuleDef();
+
+  def->addInstance("config_data_reg",
+                   "mantle.reg",
+                   {{"width", Const::make(c, 1)},
+                       {"has_en", Const::make(c, true)}});
+
+  def->addInstance("compare_addr",
+                   "coreir.eq",
+                   {{"width", Const::make(c, addr_width)}});
+
+  def->addInstance("and_op",
+                   "coreir.and",
+                   {{"width", Const::make(c, width)}});
+
+  def->addInstance("or_op",
+                   "coreir.or",
+                   {{"width", Const::make(c, width)}});
+
+  def->addInstance("op_select",
+                   "coreir.mux",
+                   {{"width", Const::make(c, width)}});
+
+  def->addInstance("out_reg",
+                   "coreir.reg",
+                   {{"width", Const::make(c, width)}});
+
+  def->addInstance("chip_addr",
+                   "coreir.const",
+                   {{"width", Const::make(c, addr_width)}},
+                   {{"value", Const::make(c, BitVec(addr_width, 12))}});
+
+  // Wire up clock
+  def->connect("self.clk", "config_data_reg.clk");
+  def->connect("self.clk", "out_reg.clk");
+
+  // Wire up data inputs
+  def->connect("self.in0", "and_op.in0");
+  def->connect("self.in1", "and_op.in1");
+
+  def->connect("self.in0", "or_op.in0");
+  def->connect("self.in1", "or_op.in1");
+  
+  // Wire up data outputs
+  def->connect("and_op.out", "op_select.in0");
+  def->connect("or_op.out", "op_select.in1");
+  def->connect("op_select.out", "out_reg.in");
+  def->connect("out_reg.out", "self.out");
+
+  // Wire up config select
+  def->connect("self.config_addr", "compare_addr.in0");
+  def->connect("chip_addr.out", "compare_addr.in1");
+  def->connect("compare_addr.out", "op_select.sel");
+
+  miniChip->setDef(def);
+
+  c->runPasses({"rungenerators", "flatten"});
+
+  auto subCircuitInstances =
+    extractSubcircuit(miniChip,
+                      {def->sel("self")->sel("config_addr"),
+                          def->sel("self")->sel("config_data"),
+                          def->sel("self")->sel("clk")});
+
+  cout << "Size of subciruit = " << subCircuitInstances.size() << endl;
+  for (auto inst : subCircuitInstances) {
+    cout << "\t" << inst->toString() << endl;
+  }
+
+  assert(subCircuitInstances.size() == 4);
+
+  deleteContext(c);
+}
+
 int main() {
   testBasicSubCircuit();
   testNodeAfterConstant();
+  testSubcircuitModule();
   testCGRAConfigSubcircuit();
 }
