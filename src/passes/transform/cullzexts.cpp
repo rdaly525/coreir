9596098fb6a5bfa28a431d832a80f1b8437bdf13
Dillon Huff <dillonhuff@gmail.com>
2018-01-15 13:35:56 -0800

    Removed cull zero extension code

--- src/passes/transform/cullzexts.cpp
+++ src/passes/transform/cullzexts.cpp
@@ -26,97 +26,97 @@ bool Passes::CullZexts::runOnModule(Module* m) {
     return false;
   }
 
-  auto def = m->getDef();
+  // auto def = m->getDef();
 
   bool deletedZext = false;
 
-  cout << "Deleting zexts in " << m->toString() << endl;
-  cout << "# of instance in " << m->toString() << " = " << def->getInstances().size() << endl;
+  // cout << "Deleting zexts in " << m->toString() << endl;
+  // cout << "# of instance in " << m->toString() << " = " << def->getInstances().size() << endl;
 
-  vector<Instance*> toDelete;
+  // vector<Instance*> toDelete;
   
-  for (auto instS : def->getInstances()) {
-    Instance* inst = instS.second;
+  // for (auto instS : def->getInstances()) {
+  //   Instance* inst = instS.second;
 
-    if (getQualifiedOpName(*inst) == "coreir.zext") {
-      auto args = inst->getModuleRef()->getGenArgs();
+  //   if (getQualifiedOpName(*inst) == "coreir.zext") {
+  //     auto args = inst->getModuleRef()->getGenArgs();
 
-      uint in_width = args.at("width_in")->get<int>();
-      uint out_width = args.at("width_out")->get<int>();
+  //     uint in_width = args.at("width_in")->get<int>();
+  //     uint out_width = args.at("width_out")->get<int>();
 
-      if (in_width == out_width) {
+  //     if (in_width == out_width) {
 
-        cout << inst->toString() << " is an identity zext" << endl;
+  //       cout << inst->toString() << " is an identity zext" << endl;
 
 
-        toDelete.push_back(inst);
-      }
-    }
-  }
+  //       toDelete.push_back(inst);
+  //     }
+  //   }
+  // }
 
-  cout << "Deleting " << toDelete.size() << " id zexts" << endl;
+  // cout << "Deleting " << toDelete.size() << " id zexts" << endl;
 
   
-  for (auto inst : toDelete) {
+  // for (auto inst : toDelete) {
 
-    Select* toReplace = inst->sel("out");
-    Select* replaceIn = inst->sel("in");
+  //   Select* toReplace = inst->sel("out");
+  //   Select* replaceIn = inst->sel("in");
 
-    vector<Select*> inValues = getSignalValues(replaceIn);
+  //   vector<Select*> inValues = getSignalValues(replaceIn);
 
-    for (auto outConn : getReceiverConnections(toReplace)) {
-      auto unpackedOutConns = unpackConnection(outConn);
+  //   for (auto outConn : getReceiverConnections(toReplace)) {
+  //     auto unpackedOutConns = unpackConnection(outConn);
 
-      if (def->hasConnection(outConn.first, outConn.second)) {
-        def->disconnect(outConn.first, outConn.second);
+  //     if (def->hasConnection(outConn.first, outConn.second)) {
+  //       def->disconnect(outConn.first, outConn.second);
 
-        for (auto conn : unpackedOutConns) {
-          cout << "\tconnecting: " << conn.first->toString() << " <---> " << conn.second->toString() << endl;
-          def->connect(conn.first, conn.second);
-        }
-      }
+  //       for (auto conn : unpackedOutConns) {
+  //         cout << "\tconnecting: " << conn.first->toString() << " <---> " << conn.second->toString() << endl;
+  //         def->connect(conn.first, conn.second);
+  //       }
+  //     }
 
-    }
+  //   }
 
-    vector<Connection> newConns;
-    for (uint i = 0; i < inValues.size(); i++) {
-      Select* replacement = inValues[i];
-      Select* toReplaceI = toReplace->sel(to_string(i));
+  //   vector<Connection> newConns;
+  //   for (uint i = 0; i < inValues.size(); i++) {
+  //     Select* replacement = inValues[i];
+  //     Select* toReplaceI = toReplace->sel(to_string(i));
 
-      if (replacement != nullptr) {
-        for (auto outConn : getReceiverConnections(toReplace)) {
+  //     if (replacement != nullptr) {
+  //       for (auto outConn : getReceiverConnections(toReplace)) {
 
-          Wireable* fst = outConn.first;
-          Wireable* snd = outConn.second;
+  //         Wireable* fst = outConn.first;
+  //         Wireable* snd = outConn.second;
 
-          Wireable* newFst = replaceSelect(toReplaceI,
-                                           replacement,
-                                           fst);
+  //         Wireable* newFst = replaceSelect(toReplaceI,
+  //                                          replacement,
+  //                                          fst);
 
-          Wireable* newSnd = replaceSelect(toReplaceI,
-                                           replacement,
-                                           snd);
+  //         Wireable* newSnd = replaceSelect(toReplaceI,
+  //                                          replacement,
+  //                                          snd);
 
-          newConns.push_back({newFst, newSnd});
+  //         newConns.push_back({newFst, newSnd});
 
-        }
-      } else {
-        // Could just silently skip but I want to see if there are any unconnected
-        // zero extends
-        assert(false);
-      }
-    }
+  //       }
+  //     } else {
+  //       // Could just silently skip but I want to see if there are any unconnected
+  //       // zero extends
+  //       assert(false);
+  //     }
+  //   }
 
-    def->removeInstance(inst);
+  //   def->removeInstance(inst);
 
-    for (auto conn : newConns) {
-      def->connect(conn.first, conn.second);
-    }
+  //   for (auto conn : newConns) {
+  //     def->connect(conn.first, conn.second);
+  //   }
 
-    deletedZext = true;
-  }
+  //   deletedZext = true;
+  // }
 
-  cout << "Done deleting zexts" << endl;
+  // cout << "Done deleting zexts" << endl;
 
   return deletedZext;
 }
