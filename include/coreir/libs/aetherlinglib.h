0e7d5438732f9512ae9bbebeb224a37f5497ddf2
David Durst <davidbdurst@gmail.com>
2018-01-19 13:52:08 -0800

    adding aetherling headder

---
+++ include/coreir/libs/aetherlinglib.h
@@ -0,0 +1,15 @@
+#ifndef COREIR_AETHERLINGLIB_H_
+#define COREIR_AETHERLINGLIB_H_
+
+#include "coreir/common-macros.h"
+#include "coreir-c/ctypes.h"
+
+#ifdef __cplusplus
+#include "coreir.h"
+COREIR_GEN_CPP_API_DECLARATION_FOR_LIBRARY(aetherlinglib);
+#endif
+
+COREIR_GEN_C_API_DECLARATION_FOR_LIBRARY(aetherlinglib);
+
+
+#endif //COREIR_AETHERLINGLIB_HPP_
