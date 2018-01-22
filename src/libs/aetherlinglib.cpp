5e733af3ebe4db20f2bfb6f5ba11f46f9cadb9b2
David Durst <davidbdurst@gmail.com>
2018-01-21 23:52:08 -0800

    first draft of map, lots of extra stuff at bottom to remove

--- src/libs/aetherlinglib.cpp
+++ src/libs/aetherlinglib.cpp
@@ -1,4 +1,7 @@
 #include "coreir/libs/aetherlinglib.h"
+#include "coreir/libs/commonlib.h"
+#include <vector>
+#include <math>
 
 COREIR_GEN_C_API_DEFINITION_FOR_LIBRARY(aetherlinglib);
 
@@ -7,37 +10,215 @@ using namespace CoreIR;
 
 Namespace* CoreIRLoadLibrary_aetherlinglib(Context* c) {
 
-  Namespace* aetherlinglib = c->newNamespace("aetherlinglib");
-  //Namespace* coreirprims = c->getNamespace("coreir");
+    Namespace* aetherlinglib = c->newNamespace("aetherlinglib");
+    Namespace* coreirprims = c->getNamespace("coreir");
 
-  /////////////////////////////////
-  // Commonlib Types
-  /////////////////////////////////
 
-  Params widthparams = Params({{"width",c->Int()}});
+    /////////////////////////////////
+    // Commonlib Types
+    /////////////////////////////////
 
-  Params mapNparams = Params({
-          {"width", c->Int()},
-          {"N", c->Int()},
-          {"numOperatorsParallel", c->Int()},
-          {"operator", c->String()}
-      });
+    Params widthparams = Params({{"width",c->Int()}});
 
-  aetherlinglib->newTypeGen(
-      "mapN_type", // name for typegen
-      mapNparams, // generator parameters
-      [](Context* c, Values genargs) { //Function to compute type
-          uint width = genargs.at("width")->get<int>();
-          uint N = genargs.at("N")->get<int>();
-          return c->Record({
-                  {"in", c->BitIn()->Arr(width)->Arr(N)},
-                  {"out", c->Bit()->Arr(width)}
-              });
-      });
+    Params mapNparams = Params({
+            {"width", c->Int()},
+            {"N", c->Int()},
+            {"numOperatorsParallel", c->Int()},
+            {"cyclesPerOperation", c->Int()},
+            {"operator", c->String()}
+        });
 
-  aetherlinglib->newGeneratorDecl("mapN", aetherlinglib->getTypeGen("mapN_type"), mapNparams);
+    aetherlinglib->newTypeGen(
+        "mapN_type", // name for typegen
+        mapNparams, // generator parameters
+        [](Context* c, Values genargs) { //Function to compute type
+            uint width = genargs.at("width")->get<int>();
+            uint N = genargs.at("N")->get<int>();
+            uint numOperatorsParallel = genargs.at("numOperatorsParallel")->get<int>();
+            return c->Record({
+                    {"in", c->BitIn()->Arr(width)->Arr(N)},
+                    {"out", c->Bit()->Arr(width)->Arr(numOperatorsParallel)}
+                });
+        });
 
-  return aetherlinglib;  
+    aetherlinglib->newGeneratorDecl("mapN", aetherlinglib->getTypeGen("mapN_type"), mapNparams);
+
+    addCounter(c, aetherlinglib);
+
+    opN->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
+            uint width = genargs.at("width")->get<int>();
+            uint N = genargs.at("N")->get<int>();
+            uint cyclesPerOperation = genargs.at("cyclesPerOperation")->get<int>();
+            uint numOperatorsParallel = genargs.at("numOperatorsParallel")->get<int>();
+            string op = genargs.at("operator")->get<string>();
+            assert(N>0);
+            assert(N >= numOperatorsParallel); // nonsensical to have more parallelism than operations
+
+            Namespace* aetherlinglib = c->getNamespace("aetherlinglib");
+            Generator* mapN = aetherlinglib->getGenerator("mapn");
+            Generator* const_gen = coreirprims->getGenerator("const");
+
+            Const* aWidth = Const::make(c,width);
+            Const* aOperator = Const::make(c,op);
+
+            uint inputsPerOperatorPreSpillover = N / numOperatorsParallel;
+            uint extraInputs = N % numOperatorsParallel;
+            uint maxInputsPerOperator = inputsPerOperatorPreSpillover + (extraInputs > 0 ? 1 : 0);
+            Const* aMaxInputsPerOperator = Const::make(c, maxInputsPerOperator);
+
+            vector<uint> inputsPerOperator(numOperators, inputsPerOperatorPreSpillover);
+
+            // clock needs to be able count from when map starts doing N operations until all N
+            // have been computed (for any amount of parallelism)
+            uint clocksToCompleteMap = log2((inputsPerOperatorPreSpillover + 1) * cyclesPerOperation);
+            def->addInstance("counterClocksPerOp", "commonlib.counter",
+                             {
+                                 {"width", Const::make(c, ((int) log2(cyclesPerOperation)) + 1)},
+                                 {"min", Const::make(c, 0)},
+                                 {"max", Const::make(c, cyclesPerOperation)},
+                                 {"inc", Const::make(c, 1)}
+                             });
+
+            def->addInstance("counterWhichInput", "commonlib.counter",
+                             {
+                                 {"width", Const::make(c, ((int) log2(numOperatorsParallel)) + 1)},
+                                 {"min", Const::make(c, 0)},
+                                 {"max", Const::make(c, numOperatorsParlale)},
+                                 {"inc", Const::make(c, 1)}
+                             });
+
+            
+            def->addInstance("always1Const", const_gen, {{"width",Const::make(c,1)}},
+                             {{"value",Const::make(c,BitVector(1,1))}});
+
+            def->connect("always1Const.out", "counterClocksPerOp.en");
+            def->connect("counterClocksPerOp.overflow", "counterWhichInput.en");
+
+//            def->addInstance("counterWhichInput", "commonlib.counter"
+            
+
+            for (int i = 0; i < inputsPerOperator.size(); i++) {
+                string idxStr = to_string(i);
+                // assign the extra inputs that don't cleanly divide by the
+                // number of operators 
+                if (i < extraInputs) {
+                    inputsPerOperator[i]++;
+                }
+
+                // for every operator, create a mux for the inputs to it, as counter goes forward,
+                // mux will pass different inputs to the operator, with changes spaced enough clock
+                // cycles apart that the operator can output the result to the next stage
+                string muxName = "muxn" + idxStr;
+                def->addInstance(muxName, "commonlib.muxn",
+                                 {{"width", aWidth}, {"N", aMaxInputsPerOperator}});
+
+                def->connect("counterWhichInput.out", muxName + ".in.sel");
+
+                for (int j = 0; j < inputsPerOperatorPreSpillover; j++) {
+                    string inputIdxStr = to_string(i*maxInputsPerOperator + j);
+                    def->connect("self.in." + inputIdxStr, muxName + ".in.data." + to_string(j));
+                }
+
+                // now create the op and wire the mux into it
+                def->addInstance("op_" + idxStr, op);
+                def->connect("mux.out", "op_" + idxStr + ".in");
+            }
+             
+            if (N == 1) {
+                def->connect("self.in.0","self.out");
+            }
+            else if (N == 2) {
+                def->addInstance("join",op2,{{"width",aWidth}});
+                def->connect("join.out","self.out");
+
+                def->connect("self.in.0","join.in0");
+                def->connect("self.in.1","join.in1");
+            }
+            else {
+                def->addInstance("join",op2,{{"width",aWidth}});
+                def->connect("join.out","self.out");
+
+                //Connect half instances
+                uint Nbits = num_bits(N-1); // 4 inputs has a max index of 3
+                uint Nlargehalf = 1 << (Nbits-1);
+                uint Nsmallhalf = N - Nlargehalf;
+
+                //cout << "N=" << N << " which has bitwidth " << Nbits << ", breaking into " << Nlargehalf << " and " << Nsmallhalf <<endl;
+                Const* aNlarge = Const::make(c,Nlargehalf);
+                Const* aNsmall = Const::make(c,Nsmallhalf);
+
+                def->addInstance("opN_0",opN,{{"width",aWidth},{"N",aNlarge},{"operator",aOperator}});
+                def->addInstance("opN_1",opN,{{"width",aWidth},{"N",aNsmall},{"operator",aOperator}});
+                for (uint i=0; i<Nlargehalf; ++i) {
+                    def->connect({"self","in",to_string(i)},{"opN_0","in",to_string(i)});
+                }
+                for (uint i=0; i<Nsmallhalf; ++i) {
+                    def->connect({"self","in",to_string(i+Nlargehalf)},{"opN_1","in",to_string(i)});
+                }
+                def->connect("opN_0.out","join.in0");
+                def->connect("opN_1.out","join.in1");
+            }
+
+        });
+
+    return aetherlinglib;  
+}
+
+void addCounter(Context* c, Namespace* ns) {
+    // counter type
+    ns->newTypeGen(
+        "counter_type", //name for the typegen
+        {{"width",c->Int()},{"min",c->Int()},{"max",c->Int()},{"inc",c->Int()}}, //generater parameters
+        [](Context* c, Values genargs) { //Function to compute type
+            uint width = genargs.at("width")->get<int>();
+            return c->Record({
+                    {"out",c->Bit()->Arr(width)},
+                    {"overflow",c->Bit()}
+                });
+        }
+        );
+
+    Generator* counter = aetherlinglib->newGeneratorDecl("counter",commonlib->getTypeGen("counter_type"),{{"width",c->Int()},{"min",c->Int()},{"max",c->Int()},{"inc",c->Int()}});
+
+    counter->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
+            uint width = genargs.at("width")->get<int>();
+            uint max = genargs.at("max")->get<int>();
+            uint min = genargs.at("min")->get<int>();
+            uint inc = genargs.at("inc")->get<int>();
+            assert(width>0);
+            assert(max>min);
+
+            // get generators
+            Namespace* coreirprims = c->getNamespace("coreir");
+            Generator* ult_gen = coreirprims->getGenerator("ult");
+            Generator* add_gen = coreirprims->getGenerator("add");
+            Generator* const_gen = coreirprims->getGenerator("const");
+
+            // create hardware
+            Const* aBitwidth = Const::make(c,width);
+            Const* aReset = Const::make(c,BitVector(width,min));
+            def->addInstance("count", "mantle.reg", {{"width",aBitwidth},{"has_clr",Const::make(c,true)},{"has_en",Const::make(c,false)}}, {{"init",aReset}});
+
+            def->addInstance("max", const_gen, {{"width",aBitwidth}}, {{"value",Const::make(c,BitVector(width,max))}});
+            def->addInstance("inc", const_gen, {{"width",aBitwidth}}, {{"value",Const::make(c,BitVector(width,inc))}});
+            def->addInstance("ult", ult_gen, {{"width",aBitwidth}});
+            def->addInstance("add", add_gen, {{"width",aBitwidth}});
+            //def->addInstance("and", "corebit.and");
+
+            // wire up modules
+            // clear if max < count+inc
+            def->connect("count.out","self.out");
+            def->connect("count.out","add.in0");
+            def->connect("inc.out","add.in1");
+
+            def->connect("add.out","count.in");
+
+            def->connect("add.out","ult.in1");
+            def->connect("max.out","ult.in0");
+            def->connect("ult.out","count.clr");
+            def->connect("ult.out","self.overflow");
+
+        });
 }
 
 COREIR_GEN_EXTERNAL_API_FOR_LIBRARY(aetherlinglib)
