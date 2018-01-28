e424bc2431ae000746c911867a6105121c2c8a99
Ross <rdaly525@stanford.edu>
2018-01-27 22:43:41 -0800

    added module param example with definition

--- tests/unit/moduleparam.cpp
+++ tests/unit/moduleparam.cpp
@@ -17,22 +17,35 @@ int main() {
       uint width = op->getGenArgs().at("width")->get<int>();
       uint N = args.at("N")->get<int>();
       return c->Record({
-        {"in",c->BitIn()->Arr(width)->Arr(N)},
-        {"out",c->Bit()->Arr(width)}
+        {"in",c->BitIn()->Arr(width)->Arr(2)->Arr(N)},
+        {"out",c->Bit()->Arr(width)->Arr(N)}
       });
     }
   );
 
 
   Generator* map = g->newGeneratorDecl("map",g->getTypeGen("map_type"),{{"N",c->Int()},{"op",ModuleType::make(c)}});
-  (void) map;
+  map->setGeneratorDefFromFun([](Context* c, Values args, ModuleDef* def) {
+    Module* op = args.at("op")->get<Module*>();
+    uint N = args.at("N")->get<int>();
+    for (uint i=0; i<N; ++i) {
+      Instance* opN = def->addInstance("op"+to_string(i),op);
+      def->connect(opN->sel("in0"),def->getInterface()->sel("in")->sel(i)->sel(0));
+      def->connect(opN->sel("in1"),def->getInterface()->sel("in")->sel(i)->sel(1));
+      def->connect(opN->sel("out"),def->getInterface()->sel("out")->sel(i));
+    }
+  });
   Module* test = c->getGlobal()->newModuleDecl("testModule",c->Record());
   ModuleDef* def = test->newModuleDef();
   Module* mul = c->getGenerator("coreir.mul")->getModule({{"width",Const::make(c,13)}});
   
-  def->addInstance("map_mul", "global.map", {{"N",Const::make(c,5)},{"op",Const::make(c,mul)}});
+  Instance* map_mul = def->addInstance("map_mul", "global.map", {{"N",Const::make(c,5)},{"op",Const::make(c,mul)}});
   test->setDef(def);
   test->print();
+  map_mul->getModuleRef()->print();
+  c->runPasses({"rungenerators"});
+  test->print();
+  map_mul->getModuleRef()->print();
   deleteContext(c);
   return 0;
 }
