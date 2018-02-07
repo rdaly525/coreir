dd202b661774d82898424fc64ce3fe8653ad3299
David Durst <davidbdurst@gmail.com>
2018-02-06 20:38:25 -0800

    removing dead code from mapN

--- src/libs/aetherlinglib/aeMapN.h
+++ src/libs/aetherlinglib/aeMapN.h
@@ -10,13 +10,11 @@ void Aetherling_createMapGenerator(Context* c) {
     Namespace* aetherlinglib = c->getNamespace(AETHERLING_NAMESPACE);
     
     /*
-     * width - the width in bits of each input
      * parallelOperatrs - how many operators to have in parallel
      * operator - the operator to parallelize. Note that it must have one input known as "in" and 
      * one output known as "out"
      */
     Params mapNparams = Params({
-            //{"width", c->Int()},
             {"parallelOperators", c->Int()},
             {"operator", ModuleType::make(c)}
         });
@@ -25,15 +23,12 @@ void Aetherling_createMapGenerator(Context* c) {
         "mapN_type", // name for typegen
         mapNparams, // generator parameters
         [](Context* c, Values genargs) { //Function to compute type
-            //uint width = genargs.at("width")->get<int>();
             uint parallelOperators = genargs.at("parallelOperators")->get<int>();
             Module* opModule = genargs.at("operator")->get<Module*>();
             RecordType* opType = opModule->getType();
             return c->Record({
                     {"in", opType->sel("in")->Arr(parallelOperators)},
                     {"out", opType->sel("out")->Arr(parallelOperators)}
-                    //{"in", c->BitIn()->Arr(width)->Arr(parallelOperators)},
-                    //{"out", c->Bit()->Arr(width)->Arr(parallelOperators)}
                 });
         });
 
@@ -41,20 +36,9 @@ void Aetherling_createMapGenerator(Context* c) {
         aetherlinglib->newGeneratorDecl("mapN", aetherlinglib->getTypeGen("mapN_type"), mapNparams);
 
     mapN->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
-            //uint width = genargs.at("width")->get<int>();
             uint parallelOperators = genargs.at("parallelOperators")->get<int>();
             Module* opModule = genargs.at("operator")->get<Module*>();
-            //Type* inputType = genargs.at("inputType")->get<Type*>();
-            //Type* outputType = genargs.at("outputType")->get<Type*>();
             assert(parallelOperators>0);
-            //assert(width>0);
-            //RecordType* inputTypeRecord = dynamic_cast<RecordType*>(inputType);
-            //assert(inputTypeRecord != 0); // 0 if cast failed, so input type wasn't RecordType
-            //RecordType* outputTypeRecord = dynamic_cast<RecordType*>(outputType);
-            //assert(outputTypeRecord != 0); // 0 if cast failed, so output type wasn't RecordType
-            //opModule->getType()->sel("in")->print();
-            //inputType->print();
-            //assert(opModule->getType()->sel("in") == inputType); // ensure input type maps module type
             
             // now create each op and wire the inputs and outputs to it
             for (uint i = 0; i < parallelOperators; i++) {
