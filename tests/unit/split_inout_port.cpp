abfc10340293882c8d85d903bd4adf1485935be5
Dillon Huff <dillonhuff@gmail.com>
2018-02-20 20:01:47 -0800

    Starting inout port splitting

---
+++ tests/unit/split_inout_port.cpp
@@ -0,0 +1,12 @@
+#include "coreir.h"
+//#include "coreir/passes/transform/split_inouts.h"
+
+using namespace CoreIR;
+using namespace std;
+
+int main() {
+  Context* c = newContext();
+
+  deleteContext(c);
+  assert(false);
+}
