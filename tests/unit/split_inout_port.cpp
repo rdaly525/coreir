a975e3e545b7f929190c329bb6c3ddee48f36b1e
Dillon Huff <dillonhuff@gmail.com>
2018-02-20 20:17:28 -0800

    Adding inout splitting

--- tests/unit/split_inout_port.cpp
+++ tests/unit/split_inout_port.cpp
@@ -1,12 +1,42 @@
 #include "coreir.h"
-//#include "coreir/passes/transform/split_inouts.h"
 
 using namespace CoreIR;
 using namespace std;
 
 int main() {
   Context* c = newContext();
+  Namespace* g = c->getGlobal();
 
+  uint width = 1;
+  
+  Type* tp = c->Record({{"io", c->BitInOut()},
+        {"en", c->BitIn()},
+          {"from_io", c->Bit()},
+            {"to_io", c->BitIn()}});
+
+  Module* io = g->newModuleDecl("io", tp);
+  ModuleDef* def = io->newModuleDef();
+
+  def->addInstance("tristate_buf",
+                   "coreir.triput",
+                   {{"width", Const::make(c, width)}});
+
+  def->addInstance("tristate_out",
+                   "coreir.triget",
+                   {{"width", Const::make(c, width)}});
+
+  def->connect("tristate_buf.en", "self.en");
+  def->connect("tristate_buf.in.0", "self.to_io");
+  def->connect("tristate_buf.out.0", "self.io");
+
+  def->connect("tristate_out.in.0", "self.io");
+  def->connect("tristate_out.out.0", "self.from_io");
+
+  io->setDef(def);
+
+  c->runPasses({"split-inouts"});
+  
   deleteContext(c);
+
   assert(false);
 }
