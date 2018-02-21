16e5d2058c69af6249f09e23139a574a07734751
Dillon Huff <dillonhuff@gmail.com>
2018-02-20 19:16:36 -0800

    About to merge upstream

--- tests/unit/tristate_conversion.cpp
+++ tests/unit/tristate_conversion.cpp
@@ -4,9 +4,17 @@ using namespace CoreIR;
 using namespace std;
 
 int main() {
-  Context* c = newContext();
 
-  assert(false);
+  // Note: I cant load rtlil here. Need to change build structure.
+
+  Context* c = newContext();
+  Namespace* g = c->getGlobal();
+
+  Type* mTp = c->Record({{"inout1", c->BitInOut()},
+        {"inout2", c->BitInOut()}});
+
+  Module* m = g->newModuleDecl("m", mTp);
+  ModuleDef* def = m->newModuleDef();
 
   deleteContext(c);
 }
