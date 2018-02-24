e5a16ccd730f45cb61308e9104b76743d8e335ab
Dillon Huff <dillonhuff@gmail.com>
2018-02-23 18:03:48 -0800

    hasOutputConnection now checks for inouts

--- tests/unit/delete_dead_instances.cpp
+++ tests/unit/delete_dead_instances.cpp
@@ -18,16 +18,16 @@ int main() {
   def->addInstance("tb", "coreir.tribuf", {{"width", Const::make(c, 1)}});
 
   def->connect("self.en", "tb.en");
-  def->connect("self.input", "tb.in");
-  def->connect("tb.out", ,"self.pad");
+  def->connect("self.input", "tb.in.0");
+  def->connect("tb.out.0", "self.pad");
 
   md->setDef(def);
+
+  assert(md->getDef()->getInstances().size() == 1);
 
   c->runPasses({"deletedeadinstances"});
 
   assert(md->getDef()->getInstances().size() == 1);
-
-  assert(false);
 
   deleteContext(c);
 }
