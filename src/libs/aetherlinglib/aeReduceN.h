24f169ad7cb5f7547962d3e15f1c19a0ca98a81b
David Durst <davidbdurst@gmail.com>
2018-02-27 23:35:05 -0800

    converted reduce into parallel and sequential versions, sequential currently array based

--- src/libs/aetherlinglib/aeReduceN.h
+++ src/libs/aetherlinglib/aeReduceN.h
@@ -9,45 +9,56 @@ string getOpName(int layer, int idxInLayer) {
     return "op_" + to_string(layer) + "_" + to_string(idxInLayer);
 }
 
+// https://stackoverflow.com/a/600306
+bool IsPowerOfTwo(uint x)
+{
+    return (x != 0) && ((x & (x - 1)) == 0);
+}
+
 void Aetherling_createReduceGenerator(Context* c) {
 
     Namespace* aetherlinglib = c->getNamespace(AETHERLING_NAMESPACE);
     
     /*
-     * numLayers - the number of layers of operators. The number of inputs will be 2^(numLayers+1), because
-     * each operator takes two inputs, so first layer of ops takes inputs equal to double its number of 
-     * operators
+     * numInputs - the number of eleemnts to reduce 
      * operator - the operator to parallelize. Note that it must have two inputs known as "in0" and "in1" and 
      * one output known as "out"
      */
-    Params reduceNparams = Params({
-            {"numLayers", c->Int()},
+    Params reduceParallelSerialParams = Params({
+            {"numInputs", c->Int()},
             {"operator", ModuleType::make(c)}
         });
 
+    /*
+     * reduceParallelPower2Inputs requires that the inputs is a power of 2
+     */
     aetherlinglib->newTypeGen(
-        "reduceN_type", // name for typegen
-        reduceNparams, // generator parameters
+        "reduceParallelPower2Inputs_type", // name for typegen
+        reduceParallelSerialParams, // generator parameters
         [](Context* c, Values genargs) { //Function to compute type
-            uint inputs = pow(2, genargs.at("numLayers")->get<int>());
+            uint numInputs = genargs.at("numInputs")->get<int>();
             Module* opModule = genargs.at("operator")->get<Module*>();
             RecordType* opType = opModule->getType();
             return c->Record({
-                    {"in", opType->sel("in0")->Arr(inputs)},
-                    {"out", opType->sel("out")}
+                    {"in", opType->sel("in0")->Arr(numInputs)},
+                    {"out", opType->sel("out")},
+                    {"ready", c->Bit()},
+                    {"valid", c->Bit()}
                 });
         });
 
-    Generator* reduceN =
-        aetherlinglib->newGeneratorDecl("reduceN", aetherlinglib->getTypeGen("reduceN_type"), reduceNparams);
+    Generator* reduceParallelPower2Inputs =
+        aetherlinglib->newGeneratorDecl("reduceParallelPower2Inputs", aetherlinglib->getTypeGen("reduceParallelPower2Inputs_type"), reduceParallelParams);
 
-    reduceN->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
-            uint numLayers = genargs.at("numLayers")->get<int>();
+    reduceParallelPower2Inputs->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
+            uint numInputs = genargs.at("numInputs")->get<int>();
+            ASSERT(IsPowerOfTwo(inputs), "numInputs is not a power of 2 for reduceParallelPower2Inputs, use the reduceParallel module for this case");
+            uint depth = int(ceil(log2(numInputs)));
             Module* opModule = genargs.at("operator")->get<Module*>();
             assert(numLayers>0);
 
-            // create each layer
-            for (uint i = 0; i < numLayers; i++) {
+            // create each layer for all dpeths other than input
+            for (uint i = 0; i < depth; i++) {
                 // since its a binary tree, each layer has 2^i elements
                 for (uint j = 0; j < pow(2, i); j++) {
                     string opStr = getOpName(i, j);
@@ -66,6 +77,10 @@ void Aetherling_createReduceGenerator(Context* c) {
                     }
                 }
             }
+            // always ready and valid
+            string enableConst = Aetherling_addCoreIRConstantModule(c, def, 1, Const::make(c, 1, 1));
+            def->connect(enableConst + ".out.0", "self.ready");
+            def->connect(enableConst + ".out.0", "self.valid");
         });
 
     /*
@@ -75,14 +90,9 @@ void Aetherling_createReduceGenerator(Context* c) {
      * operator - the operator to parallelize. Note that it must have two inputs known as "in0" and "in1" and 
      * one output known as "out"
      */
-    Params reduceNAnyInputsparams = Params({
-            {"numInputs", c->Int()},
-            {"operator", ModuleType::make(c)}
-        });
-
     aetherlinglib->newTypeGen(
-        "reduceNAnyInputs_type", // name for typegen
-        reduceNAnyInputsparams, // generator parameters
+        "reduceParallel_type", // name for typegen
+        reduceParallelSerialParams, // generator parameters
         [](Context* c, Values genargs) { //Function to compute type
             uint inputs = genargs.at("numInputs")->get<int>();
             Module* opModule = genargs.at("operator")->get<Module*>();
@@ -92,23 +102,22 @@ void Aetherling_createReduceGenerator(Context* c) {
                                 {"data", opType->sel("in0")->Arr(inputs)},
                                 {"identity", opType->sel("in0")}
                             })},
-                    {"out", opType->sel("out")}
+                    {"out", opType->sel("out")},
+                    {"ready", c->Bit()},
+                    {"valid", c->Bit()}
                 });
         });
 
-    Generator* reduceNAnyInputs =
-        aetherlinglib->newGeneratorDecl("reduceNAnyInputs", aetherlinglib->getTypeGen("reduceNAnyInputs_type"), reduceNAnyInputsparams);
+    Generator* reduceParallel =
+        aetherlinglib->newGeneratorDecl("reduceParallel", aetherlinglib->getTypeGen("reduceParallel_type"), reduceParallelParams);
 
-    reduceNAnyInputs->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
+    reduceParallel->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
             uint numInputs = genargs.at("numInputs")->get<int>();
-            // this works as numLayers is one less than depth, so don't need to divide numInputs by 2
-            // as that also doesn't count as a layer
-            uint numLayers = int(ceil(log2(numInputs)));
+            uint numInputsRoundedUpToPow2 = int(pow(2,ceil(log2(numInputs))));
             Module* opModule = genargs.at("operator")->get<Module*>();
-            assert(numLayers>0);
-
-            def->addInstance("reducer", "aetherlinglib.reduceN", {
-                    {"numLayers", Const::make(c, numLayers)},
+            
+            def->addInstance("reducer", "aetherlinglib.reduceParallelPower2Inputs", {
+                    {"numInputs", Const::make(c, numInputsRoundedUpToPow2)},
                     {"operator", Const::make(c, opModule)}
                 });
 
@@ -118,59 +127,114 @@ void Aetherling_createReduceGenerator(Context* c) {
                 def->connect("self.in.data." + iStr, "reducer.in." + iStr);
             }
             // connect identity to rest of in now, all others up to power of 2
-            for (; i < int(pow(2,ceil(log2(numInputs)))); i++) {
+            for (; i < numInputsRoundedUpToPow2; i++) {
                 def->connect("self.in.identity", "reducer.in." + to_string(i));
             }
             def->connect("reducer.out", "self.out");
+            def->connect("reducer.ready", "self.ready");
+            def->connect("reducer.valid", "self.valid");
         });
 
     aetherlinglib->newTypeGen(
-        "reduceNSerializable_type", // name for typegen
-        reduceNparams, // generator parameters
+        "reduceSequential_type", // name for typegen
+        reduceParallelSequentialParams, // generator parameters
         [](Context* c, Values genargs) { //Function to compute type
-            uint inputs = pow(2, genargs.at("numLayers")->get<int>());
+            uint inputs = genargs.at("numInputs")->get<int>();
             Module* opModule = genargs.at("operator")->get<Module*>();
             RecordType* opType = opModule->getType();
             return c->Record({
-                    {"in", opType->sel("in0")->Arr(inputs)},
+                    {"in", opType->sel("in0")->Arr(numInputs)},
                     {"out", opType->sel("out")},
-                    {"mergeCur", c->BitIn()} // set this bit if you want the current output to be merged with
-                    // the last one
+                    {"ready", c->Bit()},
+                    {"valid", c->Bit()}
                 });
         });
 
-    Generator* reduceNSerializable =
-        aetherlinglib->newGeneratorDecl(
-            "reduceNSerializable",
-            aetherlinglib->getTypeGen("reduceNSerializable_type"),
-            reduceNparams);
+    Generator* reduceSequential =
+        aetherlinglib->newGeneratorDecl("reduceSequential", aetherlinglib->getTypeGen("reduceSequential_type"), reduceSequentialParallelParams);
 
-    reduceNSerializable->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
-            uint numLayers = genargs.at("numLayers")->get<int>();
+
+    reduceSequential->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
+            uint numInputs = genargs.at("numInputs")->get<int>();
             Module* opModule = genargs.at("operator")->get<Module*>();
             RecordType* opType = opModule->getType();
-            assert(numLayers>0);
+            uint elementWidth = opType->sel("out")->getSize();
+            
+            Values streamifyParams({
+                    {"elementType", Const::make(c, opType->sel("in0"))},
+                    {"arrayLength", Const::make(c, numInputs)} 
+                });
 
-            def->addInstance("reducer", "aetherlinglib.reduceN", genargs);
-            def->connect("self.in", "reducer.in");
+            def->addInstance("streamify", "aetherlinglib.streamify", streamifyParams);
+            def->addInstance("op", opModule);
+            def->addInstance("accumulatorReg", "coreir.reg",
+                             {{"width", Const::make(c, elementWidth)}});
+            // on the first clock cycle, store first input, all other clock cycles, store
+            // f(accumulator, nextInput)
+            def->addInstance("accumulatorInputMux", "commonlib.muxn", {
+                    {"width", Const::make(c, elementWidth)},
+                    {"N", Const::make(c, 2)}});
 
-            // have a mux to switch between merged and unmerged outputs
-            def->addInstance("mergeMux", "commonlib.muxn",
-                             {{"width", Const::make(c, opType->sel("in0")->getSize())}, {"N", Const::make(c, 2)}});
-            // connect the current output directly to the merge
-            def->connect("reducer.out", "mergeMux.in.data.0");
-            // also merge the current output with the last one
-            def->addInstance("mergeOp", opModule);
-            opType->print();
-            def->addInstance("lastOutputReg", "coreir.reg", {{"width", Const::make(c, opType->sel("out")->getSize())}});
-            def->connect("reducer.out", "mergeOp.in0");
-            def->connect("lastOutputReg.out", "mergeOp.in1");
-            def->connect("mergeOp.out", "mergeMux.in.data.1");
-            // pass the current chosen ouptut (merged or unmerged) to out and back to reg so that reg
-            // can repeat over multiple passes if necessary
-            def->connect("self.mergeCur", "mergeMux.in.sel.0");
-            def->connect("mergeMux.out", "lastOutputReg.in");
-            def->connect("mergeMux.out", "self.out");
+            def->connect("self.in", "streamify.in");
+            def->connect("streamify.out", "op.in0");
+            def->connect("accumlatorReg.out", "op.in1");
+            def->connect("op.out", "accumulatorInputmux.in.data.0");
+            def->connect("self.in.0", "accumulatorInputMux.in.data.1");
+            def->connect("accumulatorInputMux.out", "accumulatorReg.in");
+            // if ready is 1 (first clock of iteration through inputs) use in 0, for accumulator
+            // value. otherwise, use output of op
+            def->connect("streamify.ready", "accumulatorInputMux.sel.0");
+        });
+
+      aetherlinglib->newTypeGen(
+        "reduceSequential_type", // name for typegen
+        reduceParallelSequentialParams, // generator parameters
+        [](Context* c, Values genargs) { //Function to compute type
+            uint inputs = genargs.at("numInputs")->get<int>();
+            Module* opModule = genargs.at("operator")->get<Module*>();
+            RecordType* opType = opModule->getType();
+            return c->Record({
+                    {"in", opType->sel("in0")->Arr(numInputs)},
+                    {"out", opType->sel("out")},
+                    {"ready", c->Bit()},
+                    {"valid", c->Bit()}
+                });
+        });
+
+    Generator* reduceSequentialMultipleClocks =
+        aetherlinglib->newGeneratorDecl("reduceSequential", aetherlinglib->getTypeGen("reduceSequential_type"), reduceSequentialParallelParams);
+
+
+    reduceSequential->setGeneratorDefFromFun([](Context* c, Values genargs, ModuleDef* def) {
+            uint numInputs = genargs.at("numInputs")->get<int>();
+            Module* opModule = genargs.at("operator")->get<Module*>();
+            RecordType* opType = opModule->getType();
+            uint elementWidth = opType->sel("out")->getSize();
+            
+            Values streamifyParams({
+                    {"elementType", Const::make(c, opType->sel("in0"))},
+                    {"arrayLength", Const::make(c, numInputs)} 
+                });
+
+            def->addInstance("streamify", "aetherlinglib.streamify", streamifyParams);
+            def->addInstance("op", opModule);
+            def->addInstance("accumulatorReg", "coreir.reg",
+                             {{"width", Const::make(c, elementWidth)}});
+            // on the first clock cycle, store first input, all other clock cycles, store
+            // f(accumulator, nextInput)
+            def->addInstance("accumulatorInputMux", "commonlib.muxn", {
+                    {"width", Const::make(c, elementWidth)},
+                    {"N", Const::make(c, 2)}});
+
+            def->connect("self.in", "streamify.in");
+            def->connect("streamify.out", "op.in0");
+            def->connect("accumlatorReg.out", "op.in1");
+            def->connect("op.out", "accumulatorInputmux.in.data.0");
+            def->connect("self.in.0", "accumulatorInputMux.in.data.1");
+            def->connect("accumulatorInputMux.out", "accumulatorReg.in");
+            // if ready is 1 (first clock of iteration through inputs) use in 0, for accumulator
+            // value. otherwise, use output of op
+            def->connect("streamify.ready", "accumulatorInputMux.sel.0");
         });    
             
 }
