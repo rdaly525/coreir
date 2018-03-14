db31c0ffd1be70a1ce4dc16111ffc7e58f18c980
David Durst <davidbdurst@gmail.com>
2018-03-13 20:11:46 -0700

    added c function for getting value coreir type

--- include/coreir-c/coreir-values.h
+++ include/coreir-c/coreir-values.h
@@ -16,6 +16,7 @@ extern COREValue* COREValueInt(COREContext* c,int i);
 extern COREValue* COREValueString(COREContext* c,char* str);
 extern COREValue* COREValueBitVector(COREContext* c, int width, uint64_t val);
 extern COREValue* COREValueModule(COREContext* cc, COREModule* mod);
+extern COREValue* COREValueCoreIRType(COREContext* cc, COREType* mod);
 
 //Arg Getter functions will assert on wrong arg type
 extern bool COREValueBoolGet(COREValue* a);
