2fd75687fd6ea9fe892ab8545ffdf8a7a4ced681
Dillon Huff <dillonhuff@gmail.com>
2018-03-21 12:15:35 -0700

    Now working on real IO port splitting

--- tests/unit/split_inout_port.cpp
+++ tests/unit/split_inout_port.cpp
@@ -3,7 +3,7 @@
 using namespace CoreIR;
 using namespace std;
 
-int main() {
+void tribuf_test() {
   Context* c = newContext();
   Namespace* g = c->getGlobal();
 
@@ -62,4 +62,69 @@ int main() {
   assert(sim.getBitVec("self.io_input") == BitVector(1, 0));
   
   deleteContext(c);
+}
+
+void io_to_io_test() {
+  Context* c = newContext();
+  Namespace* g = c->getGlobal();
+
+  uint width = 1;
+
+  {
+    Type* tp = c->Record({{"io", c->BitInOut()},
+          {"en", c->BitIn()},
+            {"from_io", c->Bit()},
+              {"to_io", c->BitIn()}});
+
+    Module* inner = g->newModuleDecl("inner", tp);
+    ModuleDef* def = inner->newModuleDef();
+
+    def->addInstance("tristate_buf",
+                     "coreir.tribuf",
+                     {{"width", Const::make(c, width)}});
+
+    def->addInstance("tristate_out",
+                     "coreir.ibuf",
+                     {{"width", Const::make(c, width)}});
+
+    def->connect("tristate_buf.en", "self.en");
+    def->connect("tristate_buf.in.0", "self.to_io");
+    def->connect("tristate_buf.out.0", "self.io");
+
+    def->connect("tristate_out.in.0", "self.io");
+    def->connect("tristate_out.out.0", "self.from_io");
+
+    inner->setDef(def);
+  }
+
+  {
+    Type* outer_tp = c->Record({{"io_port", c->BitInOut()}});
+    Module* outer = g->newModuleDecl("outer", outer_tp);
+    ModuleDef* def = outer->newModuleDef();
+    def->addInstance("inner_mod", "global.inner");
+
+    def->connect("self.io_port", "inner_mod.io");
+
+    outer->setDef(def);
+
+    cout << "PRINT OUTER" << endl;
+    outer->print();
+  }
+
+  c->runPasses({"rungenerators"});
+
+  cout << "AFTER RUNGENERATORS" << endl;
+  auto outer = g->getModule("outer");
+  outer->print();
+
+  c->runPasses({"split-inouts"});
+  
+  assert(false);
+
+  deleteContext(c);
+}
+
+int main() {
+  tribuf_test();
+  io_to_io_test();
 }
