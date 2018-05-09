f12baef3224bb3a73dcb0078ba12f8822e1e77ed
Ross <rdaly525@stanford.edu>
2018-05-09 08:37:23 -0700

    removed named types with args api from context, jsonspec, and filereader

--- src/ir/context.cpp
+++ src/ir/context.cpp
@@ -252,14 +252,6 @@ NamedType* Context::Named(string nameref) {
   return this->getNamespace(split[0])->getNamedType(split[1]);
 }
 
-NamedType* Context::Named(string nameref,Values args) {
-  checkValuesAreConst(args);
-  vector<string> split = splitRef(nameref);
-  ASSERT(this->hasNamespace(split[0]),"Missing Namespace + " + split[0]);
-  ASSERT(this->getNamespace(split[0])->hasNamedType(split[1]),"Missing Named type + " + nameref);
-  return this->getNamespace(split[0])->getNamedType(split[1],args);
-}
-
 Type* Context::Flip(Type* t) { return t->getFlipped();}
 
 Type* Context::In(Type* t) {
