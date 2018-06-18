c93c51e5c691a303ae1f29455470d262391a5f57
Ross <rdaly525@stanford.edu>
2018-06-18 08:13:12 -0700

    fleshed out the rest of the forcecast functions

--- src/ir/forcecast.cpp
+++ src/ir/forcecast.cpp
@@ -1,6 +1,7 @@
 #include "coreir/ir/value.h"
 #include "coreir/ir/valuetype.h"
 #include "coreir/ir/json.h"
+#include "coreir/ir/common.h"
 
 namespace CoreIR {
 
@@ -46,38 +47,132 @@ Value* TemplatedConst<bool>::forceCast(ValueType* vt) const {
 
 template<>
 Value* TemplatedConst<int>::forceCast(ValueType* vt) const {
-  ASSERT(0,"NYI");
-  return nullptr;
+  Context* c = vt->getContext();
+  switch(vt->getKind()) {
+    case ValueType::VTK_Bool: {
+      return Const::make(c,(bool) value);
+    }
+    case ValueType::VTK_Int: {
+      ASSERT(0,"Bad cast");
+      return nullptr;
+    }
+    case ValueType::VTK_BitVector: {
+      return ConstInt::make(c,cast<BitVectorType>(vt)->getWidth(),value);
+    }
+    case ValueType::VTK_String: {
+      ASSERT(0,"Cannot cast int to string");
+      return nullptr;
+    }
+    case ValueType::VTK_CoreIRType: {
+      ASSERT(0,"Cannot cast int to CoreIRType");
+      return nullptr;
+    }
+    case ValueType::VTK_Module: {
+      ASSERT(0,"Cannot cast int to Module");
+      return nullptr;
+    }
+    case ValueType::VTK_Json: {
+      Json jval = value;
+      return Const::make(c,jval);
+    }
+    default: {
+      ASSERT(0,"Bad cast");
+      return nullptr;
+    }
+  }
 }
 
 template<>
 Value* TemplatedConst<BitVector>::forceCast(ValueType* vt) const {
-  ASSERT(0,"NYI");
-  return nullptr;
+  Context* c = vt->getContext();
+  switch(vt->getKind()) {
+    case ValueType::VTK_Bool: {
+      return Const::make(c,(bool) value.as_native_int32());
+    }
+    case ValueType::VTK_Int: {
+      return Const::make(c,value.as_native_int32());
+    }
+    case ValueType::VTK_BitVector: {
+      ASSERT(0,"Bad cast");
+      return nullptr;
+    }
+    case ValueType::VTK_String: {
+      ASSERT(0,"Cannot cast BV to string");
+      return nullptr;
+    }
+    case ValueType::VTK_CoreIRType: {
+      ASSERT(0,"Cannot cast BV to CoreIRType");
+      return nullptr;
+    }
+    case ValueType::VTK_Module: {
+      ASSERT(0,"Cannot cast BV to Module");
+      return nullptr;
+    }
+    case ValueType::VTK_Json: {
+      ASSERT(0,"NYI, cast BV to Json");
+      return nullptr;
+    }
+    default: {
+      ASSERT(0,"Bad cast");
+      return nullptr;
+    }
+  }
 }
 
 template<>
 Value* TemplatedConst<std::string>::forceCast(ValueType* vt) const {
-  ASSERT(0,"NYI");
+  ASSERT(0,"NYI, Casting strings");
   return nullptr;
 }
 
 template<>
 Value* TemplatedConst<Type*>::forceCast(ValueType* vt) const {
-  ASSERT(0,"NYI");
+  ASSERT(0,"Cannot cast a Type*");
   return nullptr;
 }
 
 template<>
 Value* TemplatedConst<Module*>::forceCast(ValueType* vt) const {
-  ASSERT(0,"NYI");
+  ASSERT(0,"Cannot cast a Module");
   return nullptr;
 }
 
 template<>
 Value* TemplatedConst<Json>::forceCast(ValueType* vt) const {
-  ASSERT(0,"NYI");
-  return nullptr;
+  Context* c = vt->getContext();
+  switch(vt->getKind()) {
+    case ValueType::VTK_Bool: {
+      ASSERT(value.type() == Json::value_t::boolean,"Json is not a bool!");
+      return Const::make(c,value.get<bool>());
+    }
+    case ValueType::VTK_Int: {
+      ASSERT(value.type() == Json::value_t::number_integer || value.type() == Json::value_t::number_unsigned,"Json not an int");
+      return Const::make(c,value.get<int>());
+    }
+    case ValueType::VTK_BitVector: {
+      ASSERT(0,"Cannot cast Json to BV");
+      return nullptr;
+    }
+    case ValueType::VTK_String: {
+      return Const::make(c,CoreIR::toString(value));
+    }
+    case ValueType::VTK_CoreIRType: {
+      ASSERT(0,"Cannot cast Json to CoreIRType");
+      return nullptr;
+    }
+    case ValueType::VTK_Module: {
+      ASSERT(0,"Cannot cast Json to Module");
+      return nullptr;
+    }
+    case ValueType::VTK_Json: {
+      ASSERT(0,"Bad cast");
+      return nullptr;
+    }
+    default: {
+      ASSERT(0,"Bad cast");
+      return nullptr;
+    }
+  }
 }
 
 
