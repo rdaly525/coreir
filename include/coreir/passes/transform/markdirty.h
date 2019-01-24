cdb4fbe8c59e0dd53e3aa4fff24a13f406847cb3
Ross <rdaly525@stanford.edu>
2019-01-24 15:32:25 -0800

    added pass to mark analysis as dirty

---
+++ include/coreir/passes/transform/markdirty.h
@@ -0,0 +1,20 @@
+#ifndef COREIR_MARKDIRTY_HPP_
+#define COREIR_MARKDIRTY_HPP_
+
+#include "coreir.h"
+
+namespace CoreIR {
+namespace Passes {
+
+class MarkDirty : public ContextPass {
+  bool nocoreir;
+  public :
+    static std::string ID;
+    MarkDirty() : ContextPass(ID,"Forces analysis passes to rerun") {}
+    bool runOnContext(Context* c);
+};
+
+}
+}
+
+#endif
