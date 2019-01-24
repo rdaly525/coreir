187f194a97b76c623129bf53d96a2277c67735bb
Ross <rdaly525@stanford.edu>
2019-01-24 14:26:08 -0800

    added some of the float ops

--- src/libs/float.cpp
+++ src/libs/float.cpp
@@ -1,4 +1,4 @@
-#include "coreir/libs/ice40.h"
+#include "coreir/libs/float.h"
 
 COREIR_GEN_C_API_DEFINITION_FOR_LIBRARY(float);
 
@@ -53,35 +53,79 @@ using namespace CoreIR;
 
 
 Namespace* CoreIRLoadLibrary_float(Context* c) {
-    Namespace* fp = c->newNamespace("ice40");
+  Namespace* fp = c->newNamespace("float");
+  
+  Params floatParams = Params({
+    {"exp_bits",c->Int()},
+    {"frac_bits",c->Int()}
+  });
+  
+  //Common Function types
+  fp->newTypeGen(
+    "unary",
+    floatParams,
+    [](Context* c, Values args) {
+      uint exp_bits = args.at("exp_bits")->get<int>();
+      uint frac_bits = args.at("frac_bits")->get<int>();
+      uint width = 1+exp_bits+frac_bits;
+      Type* ptype = c->Bit()->Arr(width);
+      return c->Record({
+        {"in",c->Flip(ptype)},
+        {"out",ptype}
+      });
+    }
+  );
+  fp->newTypeGen(
+    "binary",
+    floatParams,
+    [](Context* c, Values args) {
+      uint exp_bits = args.at("exp_bits")->get<int>();
+      uint frac_bits = args.at("frac_bits")->get<int>();
+      uint width = 1+exp_bits+frac_bits;
+      Type* ptype = c->Bit()->Arr(width);
+      return c->Record({
+        {"in0",c->Flip(ptype)},
+        {"in1",c->Flip(ptype)},
+        {"out",ptype}
+      });
+    }
+  );
+  
+  fp->newTypeGen(
+    "binaryReduce",
+    floatParams,
+    [](Context* c, Values args) {
+      uint exp_bits = args.at("exp_bits")->get<int>();
+      uint frac_bits = args.at("frac_bits")->get<int>();
+      uint width = 1+exp_bits+frac_bits;
+      Type* ptype = c->Bit()->Arr(width);
+      return c->Record({
+        {"in0",c->Flip(ptype)},
+        {"in1",c->Flip(ptype)},
+        {"out",c->Bit()}
+      });
+    }
+  );
+  
+  vector<string> unaryOps = {"neg", "sqr"};
+  vector<string> binaryOps = {"abs", "add", "sub", "mul", "div", "rem" };
+  vector<string> binaryReduceOps = {"le","lt","ge","gt","eq"};
 
-    
-    Type* SB_LUT4Type = c->Record({{"I0", c->BitIn()},
-                                   {"I1", c->BitIn()},
-                                   {"I2", c->BitIn()},
-                                   {"I3", c->BitIn()},
-                                   {"O",  c->Bit()}});
-    Params SB_LUT4Params({{"LUT_INIT", c->BitVector(16)}});
-    ice40->newModuleDecl("SB_LUT4", SB_LUT4Type, SB_LUT4Params);
+  for (auto op : unaryOps) {
+    TypeGen* tg = fp->getTypeGen("unary");
+    fp->newGeneratorDecl(op,tg,floatParams);
+  }
+  for (auto op : binaryOps) {
+    TypeGen* tg = fp->getTypeGen("binary");
+    fp->newGeneratorDecl(op,tg,floatParams);
+  }
+  for (auto op : binaryReduceOps) {
+    TypeGen* tg = fp->getTypeGen("binaryReduce");
+    fp->newGeneratorDecl(op,tg,floatParams);
+  }
+  
+  return fp;
 
-    Type* SB_CARRYType = c->Record({{"I0", c->BitIn()},
-                                    {"I1", c->BitIn()},
-                                    {"CI", c->BitIn()},
-                                    {"CO",  c->Bit()}});
-    ice40->newModuleDecl("SB_CARRY", SB_CARRYType);
-
-    Type* SB_DFFType = c->Record({{"C", c->Named("coreir.clkIn")},
-                                  {"D", c->BitIn()},
-                                  {"Q", c->Bit()}});
-    ice40->newModuleDecl("SB_DFF", SB_DFFType);
-
-    Type* SB_DFFEType = c->Record({{"C", c->Named("coreir.clkIn")},
-                                   {"D", c->BitIn()},
-                                   {"E", c->BitIn()},
-                                   {"Q", c->Bit()}});
-    ice40->newModuleDecl("SB_DFFE", SB_DFFEType);
-
-    return ice40;
 }
 
-COREIR_GEN_EXTERNAL_API_FOR_LIBRARY(ice40)
+COREIR_GEN_EXTERNAL_API_FOR_LIBRARY(float)
