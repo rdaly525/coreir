ac9e96e8a5c2cf15513776bf46a0e1862eb7f808
Raj Setaluri <raj.setaluri@gmail.com>
2019-04-09 12:42:38 -0700

    Fix unwired status output

--- src/libs/float.cpp
+++ src/libs/float.cpp
@@ -134,7 +134,8 @@ Namespace* CoreIRLoadLibrary_float(Context* c) {
     };
     vjson["definition"] = ""
     "wire [2:0] result_x;\n"
-    "CW_fp_mult #(.sig_width(frac_bits+3), .exp_width(exp_bits), .ieee_compliance(0)) mul1 (.a({in0,3'h0}),.b({in1,3'h0}),.rnd('h0),.z({out,result_x}),.status());";
+    "wire [7:0] status;\n"
+    "CW_fp_mult #(.sig_width(frac_bits+3), .exp_width(exp_bits), .ieee_compliance(0)) mul1 (.a({in0,3'h0}),.b({in1,3'h0}),.rnd('h0),.z({out,result_x}),.status(status));";
     fp->getGenerator("mul")->getMetaData()["verilog"] = vjson;
   }
   {
@@ -145,7 +146,8 @@ Namespace* CoreIRLoadLibrary_float(Context* c) {
       "output [exp_bits+frac_bits:0] out"
     };
     vjson["definition"] = ""
-    "CW_fp_add #(.sig_width(frac_bits), .exp_width(exp_bits), .ieee_compliance(0)) add1 (.a(in0),.b(in1),.rnd('h0),.z(out),.status());";
+    "wire [7:0] status;\n"
+    "CW_fp_add #(.sig_width(frac_bits), .exp_width(exp_bits), .ieee_compliance(0)) add1 (.a(in0),.b(in1),.rnd('h0),.z(out),.status(status));";
     fp->getGenerator("add")->getMetaData()["verilog"] = vjson;
   }
 
