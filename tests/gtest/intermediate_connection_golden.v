9ec11b1135d21d5b9877c76723c2feca2d9ab465
Lenny Truong <leonardtruong@fb.com>
2019-07-11 14:35:39 -0700

    Update gold file

--- tests/gtest/intermediate_connection_golden.v
+++ tests/gtest/intermediate_connection_golden.v
@@ -3,8 +3,8 @@ module foo (input I, output O);
     assign O = I;
 endmodule
 module top (input I, output O);
-wire inst0_O__inst1_I;
-foo inst0(.I(I), .O(inst0_O__inst1_I));
-foo inst1(.I(inst0_O__inst1_I), .O(O));
+wire inst0_O;
+foo inst0(.I(I), .O(inst0_O));
+foo inst1(.I(inst0_O), .O(O));
 endmodule
 
