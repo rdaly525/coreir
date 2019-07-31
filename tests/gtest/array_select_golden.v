ac3dce9f412f5883b2898b0ba9b5a576c04fb213
Lenny Truong <lenny@stanford.edu>
2019-07-31 11:55:43 -0700

    Fixes bug in verilog logic for handling self
    The existing code strips all references to `self` in names, however this
    is bad in the case that `self` is actualy part of the name (e.g. a port
    named `self_I`).  This updates the logic to only remove `self` when it
    is the first element of a select path, rather than search/replacing the
    entire select string.

--- tests/gtest/array_select_golden.v
+++ tests/gtest/array_select_golden.v
@@ -2,11 +2,11 @@
 module foo (input [3:0] I, output [3:0] O);
     assign O = I;
 endmodule
-module top (input [3:0] I, output [3:0] O);
+module top (output [3:0] O, input [3:0] self_I);
 wire [3:0] inst0_O;
 wire [3:0] inst1_O;
-foo inst0(.I({I[0],I[0],I[1],I[2]}), .O(inst0_O));
-foo inst1(.I({inst0_O[0],inst0_O[1],inst0_O[1],I[1]}), .O(inst1_O));
-assign O = {inst1_O[0],inst1_O[0],I[0],I[1]};
+foo inst0(.I({self_I[0],self_I[0],self_I[1],self_I[2]}), .O(inst0_O));
+foo inst1(.I({inst0_O[0],inst0_O[1],inst0_O[1],self_I[1]}), .O(inst1_O));
+assign O = {inst1_O[0],inst1_O[0],self_I[0],self_I[1]};
 endmodule
 
