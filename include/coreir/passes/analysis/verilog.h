cf000705a5702a94d0f417bc59e8139a53cf2d00
Lenny Truong <lenny@stanford.edu>
2019-07-31 15:31:27 -0700

    Add support for verilator_debug

--- include/coreir/passes/analysis/verilog.h
+++ include/coreir/passes/analysis/verilog.h
@@ -1,10 +1,10 @@
 #ifndef COREIR_VERILOG_HPP_
 #define COREIR_VERILOG_HPP_
 
-#include <memory>
-#include <ostream>
 #include "coreir.h"
 #include "verilogAST.hpp"
+#include <memory>
+#include <ostream>
 
 namespace vAST = verilogAST;
 
@@ -12,44 +12,49 @@ namespace CoreIR {
 namespace Passes {
 
 class Verilog : public InstanceGraphPass {
-    bool _inline = false;
-    bool verilator_debug = true;
+  bool _inline = false;
+  bool verilator_debug = true;
 
-    // We store a vector of module name, module AST node pairs to support
-    // serializing to a single or multiple files
-    std::vector<std::pair<std::string, std::unique_ptr<vAST::AbstractModule>>>
-        modules;
+  // We store a vector of module name, module AST node pairs to support
+  // serializing to a single or multiple files
+  std::vector<std::pair<std::string, std::unique_ptr<vAST::AbstractModule>>>
+      modules;
 
-    // Externally defined modules (no moduleDef), for now we just emit comments
-    // listing them when compiling to a single file
-    std::vector<Module*> extern_modules;
+  // Externally defined modules (no moduleDef), for now we just emit comments
+  // listing them when compiling to a single file
+  std::vector<Module *> extern_modules;
 
-    // Set used to track generators that are compiled as parametrized verilog
-    // modules. These parametrized modules have been instanced to create coreir
-    // modules, but we only need to compile the verilog definition once
-    std::set<Generator*> verilog_generators_seen;
+  // Set used to track generators that are compiled as parametrized verilog
+  // modules. These parametrized modules have been instanced to create coreir
+  // modules, but we only need to compile the verilog definition once
+  std::set<Generator *> verilog_generators_seen;
 
-    void compileModule(Module* module);
+  void compileModule(Module *module);
 
-   public:
-    static std::string ID;
-    Verilog() : InstanceGraphPass(ID, "Compiles IR to Verilog files", true) {}
-    ~Verilog(){};
-    bool runOnInstanceGraphNode(InstanceGraphNode& node) override;
-    void initialize(int argc, char** argv) override;
-    void setAnalysisInfo() override {
-        onlyTop = true;
-        addDependency(
-            "verifyconnectivity --onlyinputs");  // Should change back to check
-                                                 // all connections
-        addDependency("verifyflattenedtypes");
-    }
+    std::vector<std::unique_ptr<vAST::AbstractPort>>
+    compilePorts(RecordType *record_type);
 
-    void writeToStream(std::ostream& os);
-    void writeToFiles(const std::string& dir,
-                      std::unique_ptr<std::string> product_file);
+  std::unique_ptr<vAST::AbstractModule>
+  compileStringBodyModule(json verilog_json, std::string name, Module *module);
+
+public:
+  static std::string ID;
+  Verilog() : InstanceGraphPass(ID, "Compiles IR to Verilog files", true) {}
+  ~Verilog(){};
+  bool runOnInstanceGraphNode(InstanceGraphNode &node) override;
+  void initialize(int argc, char **argv) override;
+  void setAnalysisInfo() override {
+    onlyTop = true;
+    addDependency("verifyconnectivity --onlyinputs"); // Should change back to
+                                                      // check all connections
+    addDependency("verifyflattenedtypes");
+  }
+
+  void writeToStream(std::ostream &os);
+  void writeToFiles(const std::string &dir,
+                    std::unique_ptr<std::string> product_file);
 };
 
-}  // namespace Passes
-}  // namespace CoreIR
+} // namespace Passes
+} // namespace CoreIR
 #endif
