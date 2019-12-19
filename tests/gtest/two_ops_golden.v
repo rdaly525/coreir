9b5a1186cae9111417cf003fa10e9a4d8a6e6c0f
Lenny Truong <leonardtruong@fb.com>
2019-12-18 16:47:21 -0800

    Inline outputs

--- tests/gtest/two_ops_golden.v
+++ tests/gtest/two_ops_golden.v
@@ -3,14 +3,10 @@ assign O = (({0,0,0,0,0,0,0,CIN}) + I0) + I1;
 endmodule
 
 module Sub8 (input [7:0] I0, input [7:0] I1, output [7:0] O);
-wire [7:0] inst1_O;
-Add8_cin inst1(.CIN(1), .I0(I0), .I1(~ I1), .O(inst1_O));
-assign O = inst1_O;
+Add8_cin inst1(.CIN(1), .I0(I0), .I1(~ I1), .O(O));
 endmodule
 
 module test_two_ops (input [7:0] I0, input [7:0] I1, output [7:0] O);
-wire [7:0] inst1_O;
-Sub8 inst1(.I0(I0 + I1), .I1(I0), .O(inst1_O));
-assign O = inst1_O;
+Sub8 inst1(.I0(I0 + I1), .I1(I0), .O(O));
 endmodule
 
