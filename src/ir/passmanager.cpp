6b61f247d43c3ac3b1c183acf1437f6c8744db0d
Leonard (Lenny) Truong <lenny@stanford.edu>
2020-02-11 18:16:46 -0800

    Ignore mismatched-new-delete

--- src/ir/passmanager.cpp
+++ src/ir/passmanager.cpp
@@ -123,7 +123,10 @@ bool PassManager::runPass(Pass* p,vector<string>& pArgs) {
   if (argc > 1) {
     p->initialize(argc,argv);
   }
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wmismatched-new-delete"
   delete argv;
+  #pragma clang diagnostic pop
   bool modified = false;
   switch(p->getKind()) {
     case Pass::PK_Context:
