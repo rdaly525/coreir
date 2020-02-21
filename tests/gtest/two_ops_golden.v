ca5b4e88604e8f0d9caefbe6d5394688f7ee88c7
Lenny Truong <leonardtruong@fb.com>
2020-02-21 13:44:58 -0800

    Update gold files for linebreaks

--- tests/gtest/two_ops_golden.v
+++ tests/gtest/two_ops_golden.v
@@ -1,12 +1,34 @@
-module Add8_cin (input [7:0] I0, input [7:0] I1, output [7:0] O, input CIN);
+module Add8_cin (
+    input [7:0] I0,
+    input [7:0] I1,
+    output [7:0] O,
+    input CIN
+);
 assign O = (({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,CIN}) + I0) + I1;
 endmodule
 
-module Sub8 (input [7:0] I0, input [7:0] I1, output [7:0] O);
-Add8_cin inst1(.I0(I0), .I1(~ I1), .O(O), .CIN(1'b1));
+module Sub8 (
+    input [7:0] I0,
+    input [7:0] I1,
+    output [7:0] O
+);
+Add8_cin inst1 (
+    .I0(I0),
+    .I1(~ I1),
+    .O(O),
+    .CIN(1'b1)
+);
 endmodule
 
-module test_two_ops (input [7:0] I0, input [7:0] I1, output [7:0] O);
-Sub8 inst1(.I0(I0 + I1), .I1(I0), .O(O));
+module test_two_ops (
+    input [7:0] I0,
+    input [7:0] I1,
+    output [7:0] O
+);
+Sub8 inst1 (
+    .I0(I0 + I1),
+    .I1(I0),
+    .O(O)
+);
 endmodule
 
