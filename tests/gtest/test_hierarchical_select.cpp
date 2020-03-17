3c3d73ea11edfc7b6d5c02ec5bf5efa60f34969e
Lenny Truong <lenny@stanford.edu>
2020-03-17 09:50:07 -0700

    Add missing test files

---
+++ tests/gtest/test_hierarchical_select.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include "assert_pass.h"
+#include "coreir.h"
+#include "coreir/definitions/coreVerilog.hpp"
+#include "coreir/definitions/corebitVerilog.hpp"
+
+using namespace CoreIR;
+
+namespace {
+
+void load_file(Context *context, std::string file_name) {
+    Module *top;
+    bool result = loadFromFile(context, file_name, &top);
+    ASSERT_TRUE(result);
+    ASSERT_NE(top, nullptr);
+    context->setTop(top->getRefName());
+}
+
+void check_verilog(Context *context, std::string gold_file) {
+    context->runPasses({"flattentypes", "verilog --inline"}, {});
+    assertPassEq<Passes::Verilog>(context, gold_file);
+}
+
+TEST(HierarchicalSelectTest, TestHierarchicalSelectBasic) {
+    Context *c = newContext();
+    CoreIRLoadVerilog_corebit(c);
+    load_file(c, "srcs/hierarchical_select.json");
+
+    check_verilog(c, "golds/hierarchical_select.v");
+    deleteContext(c);
+}
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
