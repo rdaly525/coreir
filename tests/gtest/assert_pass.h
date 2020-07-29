9e0aec7e6f4658ff5fba86c7508087038ccb9ec7
Leonard (Lenny) Truong <lenny@stanford.edu>
2020-07-29 09:33:00 -0700

    Adds flattentypes ndarray arg (#925)
    * Adds flattentypes2 pass
    
    This preserves multi-dimensional arrays of bit/bits in the flattening
    pass (so we can code generate multi-dimensional arrays in verilog).  I
    was originally going to call this "flattentuples", but it's not quite
    that because if we have an array of tuples, we do the old flattening
    process, so there are still some arrays that are flattened.  I think one
    way to handle this might be to have a pass before this that transforms
    arrays of tuples into tuples of arrays, so for now I'll check this
    version in since this will get us started with the common case
    (multi-dimensional array of bits) and leave the multi-dimensional
    array of tuple case for later.
    
    * use a flattentypes arg instead
    
    * Revert subclass pattern

--- tests/gtest/assert_pass.h
+++ tests/gtest/assert_pass.h
@@ -6,6 +6,7 @@
 #include "coreir/passes/common.h"
 
 void assertPassEq(CoreIR::Context* c, std::string ID, std::string golden_path) {
+  if (ID == "coreirjson") { c->runPasses({"coreirjson"}, {"global"}); }
   auto pass = c->getPassManager()->getAnalysisPass(ID);
   std::ostringstream stream;
   pass->writeToStream(stream);
