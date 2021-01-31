6f2fa55fd7076637002f74597957ecb08f1ea083
Ross <rdaly525@stanford.edu>
2021-01-30 19:08:12 -0800

    bug fix

--- src/ir/moduledef.cpp
+++ src/ir/moduledef.cpp
@@ -28,12 +28,12 @@ ModuleDef::~ModuleDef() {
   for (auto item : connMetaData) delete item.second;
 }
 
-const std::map<std::string, Wireable*>& ModuleDef::getInstances(bool include_interface) const {
+const std::map<std::string, Wireable*> ModuleDef::getInstances(bool include_interface) const {
   std::map<std::string, Wireable*> instances_(this->instances.begin(), this->instances.end());
   if (include_interface) {
     instances_["self"] = this->interface;
   }
-  return std::move(instances_);
+  return instances_;
 }
 //
 const std::vector<Connection> ModuleDef::getSortedConnections(void) const {
