562fbca6be639320fac56c772a07de08c01a229a
rsetaluri <rsetaluri@users.noreply.github.com>
2021-02-11 22:14:02 -1000

    Add unused() function (#983)

---
+++ include/coreir/common/unused.hpp
@@ -0,0 +1,11 @@
+#ifndef COREIR_COMMON_UNUSED_HPP_
+#define COREIR_COMMON_UNUSED_HPP_
+
+namespace common {
+
+template<typename T> inline void unused(const T&) {};
+
+}  // namespace common
+
+
+#endif  // COREIR_COMMON_UNUSED_HPP_
