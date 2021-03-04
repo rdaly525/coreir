79a378b74a552c766b88384d0709021b822ca51a
Raj Setaluri <raj.setaluri@gmail.com>
2021-03-03 18:04:19 -0600

    Update port name interface
    This change removes the output module name from the set/getPortName
    interface.

--- include/coreir/ir/symbol_table_interface.hpp
+++ include/coreir/ir/symbol_table_interface.hpp
@@ -19,13 +19,12 @@ class SymbolTableInterface {
   virtual void setPortName(
       std::string in_module_name,
       std::string in_port_name,
-      std::string out_module_name,
       std::string out_instance_name) = 0;
 
   virtual std::string getModuleName(std::string in_module_name) const = 0;
   virtual std::string getInstanceName(
       std::string in_module_name, std::string in_instance_name) const = 0;
-  virtual std::pair<std::string, std::string> getPortName(
+  virtual std::string getPortName(
       std::string in_module_name, std::string in_port_name) const = 0;
 
   virtual ::nlohmann::json json() const = 0;
