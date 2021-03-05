9b2cce8865a99af495d577eb653e6de391fd3fb4
Raj Setaluri <raj.setaluri@gmail.com>
2021-03-04 18:05:41 -0600

    Add inline instance API
    This change just adds the APIs and nec. data structures for keeping
    track of inlining.

--- include/coreir/ir/coreir_symbol_table.hpp
+++ include/coreir/ir/coreir_symbol_table.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <array>
 #include <map>
 #include <string>
 #include "coreir/ir/symbol_table_interface.hpp"
 
 namespace CoreIR {
+
+SymbolTableSentinel* const symbolTableInlinedInstanceSentinel();
 
 class CoreIRSymbolTable : public SymbolTableInterface {
  public:
@@ -17,25 +20,40 @@ class CoreIRSymbolTable : public SymbolTableInterface {
     std::string in_module_name,
     std::string in_instance_name,
     std::string out_instance_name) override;
+  void setInstanceName(
+    std::string in_module_name,
+    std::string in_instance_name,
+    SymbolTableSentinel* const out_instance_name) override;
   void setPortName(
     std::string in_module_name,
     std::string in_port_name,
     std::string out_port_name) override;
+  void setInlineInstanceName(
+      std::string in_module_name,
+      std::string in_parent_instance_name,
+      std::string in_child_instance_name,
+      std::string out_instance_name) override;
 
   std::string getModuleName(std::string in_module_name) const override;
-  std::string getInstanceName(
+  InstanceNameType getInstanceName(
     std::string in_module_name, std::string in_instance_name) const override;
   std::string getPortName(
     std::string in_module_name, std::string in_port_name) const override;
+  std::string getInlinedInstanceName(
+      std::string in_module_name,
+      std::string in_parent_instance_name,
+      std::string in_child_instance_name) const override;
 
   ::nlohmann::json json() const override;
 
  private:
-  using StringPair = std::pair<std::string, std::string>;
+  using StringPair = std::array<std::string, 2>;
+  using StringTriple = std::array<std::string, 3>;
 
   std::map<std::string, std::string> moduleNames = {};
-  std::map<StringPair, std::string> instanceNames = {};
+  std::map<StringPair, InstanceNameType> instanceNames = {};
   std::map<StringPair, std::string> portNames = {};
+  std::map<StringTriple, std::string> inlinedInstanceNames = {};
 };
 
 }  // namespace CoreIR
