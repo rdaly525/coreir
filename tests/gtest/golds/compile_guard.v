cad1d535d133f0e72b95acf5f521e24588b257d1
Leonard (Lenny) Truong <lenny@stanford.edu>
2021-03-08 15:04:13 -0800

    Adds support for outputs inside a compile guard (#992)
    By default, we drive outputs to be 0 inside the else clause (should work
    for arbitrary ports since types have been flattened).
    
    This allows for "stubs" to be inserted when a compile_guard is not
    active.
    
    This feature is simpler than supporting a general output handler in the
    compile guard, since then we'd need to support multiple drivers from
    different compile guard cases which is tricky since in magma/coreir we
    only allow a single driver for a port.  This covers the use case at FB
    without having to dive into the details of general output handling.

--- tests/gtest/golds/compile_guard.v
+++ tests/gtest/golds/compile_guard.v
@@ -4,6 +4,13 @@ module foo_corebit_term (
 
 endmodule
 
+module foo_ASSERT_ON_compile_guard_with_output (
+    input I,
+    output O
+);
+assign O = I;
+endmodule
+
 module foo_ASSERT_ON_compile_guard (
     input I
 );
@@ -16,16 +23,20 @@ module foo__Top (
     input I,
     output O
 );
+wire ASSERT_ON_compile_guard_O;
 `ifdef ASSERT_ON
-foo_ASSERT_ON_compile_guard ASSERT_ON_compile_guard (
-    .I(I)
+foo_ASSERT_ON_compile_guard_with_output ASSERT_ON_compile_guard (
+    .I(I),
+    .O(ASSERT_ON_compile_guard_O)
 );
+`else
+assign ASSERT_ON_compile_guard_O = 0;
 `endif
 `ifndef ASSERT_ON
 foo_ASSERT_ON_compile_guard ASSERT_ON_compile_guard_undefined (
     .I(I)
 );
 `endif
-assign O = I;
+assign O = ASSERT_ON_compile_guard_O;
 endmodule
 
