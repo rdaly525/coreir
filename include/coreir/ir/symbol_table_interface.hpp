b295c1bba86c43b7514a6629fc9b94834d4e5446
Raj Setaluri <raj.setaluri@gmail.com>
2021-03-16 23:40:24 -0500

    Support multi-level inlining
    This commit adds support to the SymbolTableInterface API to allow for
    multi-level inlining. It does this by making the return type of
    get_inlined_instance_name() to be Union[str, Sentinel] (just like
    get_instance_name()). We also update the internal data structures to
    support storing this union, as well as ser/deser.
    
    NOTE: We still have not instrumented the inline pass.

--- include/coreir/ir/symbol_table_interface.hpp
+++ include/coreir/ir/symbol_table_interface.hpp
@@ -44,13 +44,18 @@ class SymbolTableInterface {
       std::string in_parent_instance_name,
       std::string in_child_instance_name,
       std::string out_instance_name) = 0;
+  virtual void setInlineInstanceName(
+      std::string in_module_name,
+      std::string in_parent_instance_name,
+      std::string in_child_instance_name,
+      SymbolTableSentinel* const out_instance_name) = 0;
 
   virtual std::string getModuleName(std::string in_module_name) const = 0;
   virtual InstanceNameType getInstanceName(
       std::string in_module_name, std::string in_instance_name) const = 0;
   virtual std::string getPortName(
       std::string in_module_name, std::string in_port_name) const = 0;
-  virtual std::string getInlinedInstanceName(
+  virtual InstanceNameType getInlinedInstanceName(
       std::string in_module_name,
       std::string in_parent_instance_name,
       std::string in_child_instance_name) const = 0;
