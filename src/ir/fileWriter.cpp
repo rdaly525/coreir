48e218c42a8b61f46e31ea8b5c971b7f850321b7
Jeff Setter <setter@stanford.edu>
2021-07-01 13:48:52 -0700

    Update coreir_to_dot to output op instead of names

--- src/ir/fileWriter.cpp
+++ src/ir/fileWriter.cpp
@@ -60,7 +60,29 @@ bool ModuleToDot(Module* m, std::ostream& stream) {
   DirectedModule* dMod = m->newDirectedModule();
   if (!dMod->getInstances().empty()) {
     for (auto inst : dMod->getInstances()) {
-      stream << "\"" << (*inst)->getInstname() << "\"; ";
+      //stream << "\"" << (*inst)->getInstname() << "\"; ";
+      string moduleName = (*inst)->getModuleRef()->getName();
+      string displayName = moduleName;
+      if (moduleName == "PE") {
+        //cout << "processing " << (*inst)->getInstname() << endl;
+        if ((*inst)->getModArgs().count("alu_op") > 0) {
+          displayName = (*inst)->getModArgs().at("alu_op")->get<string>();
+        } else if ((*inst)->getModArgs().count("lut_value") > 0) {
+          auto lutValue = (*inst)->getModArgs().at("lut_value")->get<BitVector>();
+          displayName = "LUT: " + lutValue.hex_string();
+        }
+      } else if (moduleName == "reg") {
+        displayName = "__/\\\\__";
+          
+      } else if (moduleName == "const") {
+        //int width = (*inst)->getModArgs().count("width") > 0 ? (*inst)->getModArgs().at("width")->get<int>() : 1;
+        //displayName += width;
+        if ((*inst)->getModArgs().count("value") > 0) {
+          auto bv = (*inst)->getModArgs().at("value")->get<BitVector>();
+          displayName += ": " + bv.hex_string();
+        }
+      }
+      stream << "\"" << (*inst)->getInstname() << "\" [label=\"" << displayName << "\"]; ";
     }
     stream << endl;
 
