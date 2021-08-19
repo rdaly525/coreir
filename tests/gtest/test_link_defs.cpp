1f5513760ddec1e10ba3b459158a2f04c11ef9a9
Lenny Truong <leonardtruong@fb.com>
2021-08-19 17:36:54 -0600

    Init test

---
+++ tests/gtest/test_link_defs.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "assert_pass.h"
+#include "coreir.h"
+#include "coreir/definitions/coreVerilog.hpp"
+#include "coreir/definitions/corebitVerilog.hpp"
+#include "coreir/libs/commonlib.h"
+
+using namespace CoreIR;
+
+namespace {
+
+TEST(VerilogTests, TestStringModule) {
+  Context* c = newContext();
+  CoreIRLoadVerilog_coreir(c);
+  Module* top;
+
+  if (!loadFromFile(c, "srcs/link_def.json", &top)) { c->die(); }
+  assert(top != nullptr);
+  c->setTop(top->getRefName());
+
+  const std::vector<std::string> passes = {
+    "verilog --inline"};
+  c->runPasses(passes, {});
+  assertPassEq(c, "verilog", "golds/link_def.v");
+  deleteContext(c);
+}
+}
