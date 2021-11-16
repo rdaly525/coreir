130566b3cd0aac5d013668267108217dfc25e502
Ross Daly <rdaly525@stanford.edu>
2021-11-16 04:49:32 -0800

    fixes small bug with 'Arg' in a generated modarg (#1022)
    * fixes small bug with 'Arg' in a generated modarg
    
    * Trigger Build
    
    * allows example to skip verilog gen

--- tests/binary/test_examples.py
+++ tests/binary/test_examples.py
@@ -9,8 +9,48 @@ for example in os.listdir('examples'):
         continue
     examples.append(example)
 
-@pytest.mark.parametrize("example",examples)
+
+@pytest.mark.parametrize("example", examples)
 def test_examples(example):
+    example_split = example.split('.')
+    name = example_split[0]
+
+    libs = "-l commonlib,float,float_CW"
+    #Test input parsing and serializing to json
+    res = delegator.run(f"bin/coreir -i examples/{example} {libs} -o examples/build/{name}.json")
+    assert not res.return_code, res.out + res.err
+
+    #Test syntax of serialized json
+    res = delegator.run(f"bin/coreir -i examples/build/{name}.json {libs}")
+    assert not res.return_code, res.out + res.err
+
+    #Test serializing to verilog
+    res = delegator.run(f"bin/coreir -i examples/{example} {libs} -p wireclocks-clk -o examples/build/{name}.sv")
+    assert not res.return_code, res.out + res.err
+
+    #Verify verilog syntax
+    res = delegator.run(f"verilator --lint-only examples/build/{name}.sv")
+    assert not res.return_code, res.out + res.err
+
+    #Test serializing to verilog (inlined)
+    #TODO this hangs sometimes for some examples
+    #res = delegator.run(f"bin/coreir -i examples/{example} -o examples/build/{name}_inline.v --inline")
+    #assert not res.return_code, res.out + res.err
+
+    #Verify verilog syntax (inlined)
+    #res = delegator.run(f"verilator --lint-only examples/build/{name}_inline.v")
+    #assert not res.return_code, res.out + res.err
+
+
+examples_no_verilog = []
+for example in os.listdir('examples/no_verilog'):
+    example_split = example.split('.')
+    if len(example_split) !=2 or example_split[-1] != 'json':
+        continue
+    examples_no_verilog.append(example)
+
+@pytest.mark.parametrize("example", examples_no_verilog)
+def test_examples_noverilog(example):
         example_split = example.split('.')
         name = example_split[0]
 
@@ -22,20 +62,3 @@ def test_examples(example):
         #Test syntax of serialized json
         res = delegator.run(f"bin/coreir -i examples/build/{name}.json {libs}")
         assert not res.return_code, res.out + res.err
-
-        #Test serializing to verilog
-        res = delegator.run(f"bin/coreir -i examples/{example} {libs} -p wireclocks-clk -o examples/build/{name}.sv")
-        assert not res.return_code, res.out + res.err
-
-        #Verify verilog syntax
-        res = delegator.run(f"verilator --lint-only examples/build/{name}.sv")
-        assert not res.return_code, res.out + res.err
-
-        #Test serializing to verilog (inlined)
-        #TODO this hangs sometimes for some examples
-        #res = delegator.run(f"bin/coreir -i examples/{example} -o examples/build/{name}_inline.v --inline")
-        #assert not res.return_code, res.out + res.err
-
-        #Verify verilog syntax (inlined)
-        #res = delegator.run(f"verilator --lint-only examples/build/{name}_inline.v")
-        #assert not res.return_code, res.out + res.err
